702c0f6afb3ee11b2901a5d9b1d69532e2a34e5a
john.fallows <john.fallows@kaazing.wan>
2012-07-20 21:27:58 +0000

    Move Mina Netty TCP adaptation layer into mina.netty project.
    git-svn-id: http://svn.kaazing.wan/mina.netty/trunk@36835 3c80e27c-cc2e-0410-9855-a9eb4b69377c

---
+++ src/main/java/com/kaazing/mina/netty/socket/SocketChannelIoAcceptor.java
@@ -0,0 +1,60 @@
+package com.kaazing.mina.netty.socket;
+
+import java.net.InetSocketAddress;
+
+import org.apache.mina.core.service.DefaultTransportMetadata;
+import org.apache.mina.core.service.TransportMetadata;
+import org.apache.mina.transport.socket.SocketAcceptor;
+import org.apache.mina.transport.socket.SocketSessionConfig;
+import org.jboss.netty.channel.socket.ServerSocketChannelFactory;
+
+import com.kaazing.mina.netty.ChannelIoAcceptor;
+import com.kaazing.mina.netty.DefaultIoAcceptorChannelHandlerFactory;
+import com.kaazing.mina.netty.IoAcceptorChannelHandlerFactory;
+
+public class SocketChannelIoAcceptor extends ChannelIoAcceptor<SocketSessionConfig, IoAcceptorSocketChannelFactory, InetSocketAddress> implements SocketAcceptor {
+
+	private static final TransportMetadata TRANSPORT_METADATA = new DefaultTransportMetadata(
+			"Kaazing", "SocketChannel", false, true, InetSocketAddress.class,
+			SocketSessionConfig.class, Object.class);
+	
+	public SocketChannelIoAcceptor(SocketSessionConfig sessionConfig,
+			final ServerSocketChannelFactory channelFactory) {
+		super(sessionConfig, new IoAcceptorSocketChannelFactory(channelFactory), new DefaultIoAcceptorChannelHandlerFactory());
+	}
+
+	public SocketChannelIoAcceptor(SocketSessionConfig sessionConfig,
+			final ServerSocketChannelFactory channelFactory, IoAcceptorChannelHandlerFactory handlerFactory) {
+		super(sessionConfig, new IoAcceptorSocketChannelFactory(channelFactory), handlerFactory);
+	}
+
+	@Override
+	public int getBacklog() {
+		return getChannelFactory().getBacklog();
+	}
+
+	@Override
+	public void setBacklog(int backlog) {
+		getChannelFactory().setBacklog(backlog);
+	}
+	
+	@Override
+	public boolean isReuseAddress() {
+		return getChannelFactory().isReuseAddress();
+	}
+
+	@Override
+	public void setReuseAddress(boolean reuseAddress) {
+		getChannelFactory().setReuseAddress(reuseAddress);
+	}
+
+	@Override
+	public void setDefaultLocalAddress(InetSocketAddress localAddress) {
+		super.setDefaultLocalAddress(localAddress);
+	}
+
+	@Override
+	public TransportMetadata getTransportMetadata() {
+		return TRANSPORT_METADATA;
+	}
+}
