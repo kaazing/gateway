f4f4e924b61b5a9eaf9cd460301bac22da340fb5
chris.barrow <chris.barrow@kaazing.wan>
2013-03-06 20:34:37 +0000

    (merge to mina.netty/branches/features/KG_7260) (KG-7260) Add writeComplete method to IoSessionChannelHandler to update (decrease) scheduledWriteBytes on the (Abstract)IoSession when data is written out to the network.
    git-svn-id: http://svn.kaazing.wan/mina.netty/branches/features/KG_7260@56257 3c80e27c-cc2e-0410-9855-a9eb4b69377c

--- src/main/java/com/kaazing/mina/netty/IoSessionChannelHandler.java
+++ src/main/java/com/kaazing/mina/netty/IoSessionChannelHandler.java
@@ -8,11 +8,13 @@ import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.future.IoFuture;
 import org.apache.mina.core.session.IoSessionInitializer;
 import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ChannelStateEvent;
 import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
+import org.jboss.netty.channel.WriteCompletionEvent;
 
 public class IoSessionChannelHandler extends SimpleChannelHandler {
 
@@ -60,5 +62,11 @@ public class IoSessionChannelHandler extends SimpleChannelHandler {
 		}
 		session.getFilterChain().fireMessageReceived(message);
 	}
+
+    public void writeComplete(
+            ChannelHandlerContext ctx, WriteCompletionEvent e) throws Exception {
+        int writtenBytes = (int)e.getWrittenAmount();
+        session.increaseScheduledWriteBytes(-writtenBytes);
+    }
 
 }
