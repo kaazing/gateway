5e0ea458e3c953724e50eadfe8e80f4d52f5a2f5
chris.barrow <chris.barrow@kaazing.wan>
2013-03-15 20:43:57 +0000

    (merge to mina.netty/branches/features/thread_alignment) (KG-7129) Flesh out DatagramChannelIoAcceptor, add NioDatagramChannelIoConnector, NioDatagramChannelIoSession, add integration test DatagramIT. NOTE: still not working, ChannelIoAcceptor needs changing to push down the connect-specific stuff (because we can't use a ServerBootstrap for connectionless transport, and there are no child channels for connectionless).
    git-svn-id: http://svn.kaazing.wan/mina.netty/branches/features/thread_alignment@56791 3c80e27c-cc2e-0410-9855-a9eb4b69377c

--- src/main/java/com/kaazing/mina/netty/nio/NioDatagramChannelIoConnector.java
+++ src/main/java/com/kaazing/mina/netty/nio/NioDatagramChannelIoConnector.java
@@ -8,28 +8,25 @@ import java.net.InetSocketAddress;
 
 import org.apache.mina.core.service.DefaultTransportMetadata;
 import org.apache.mina.core.service.TransportMetadata;
-import org.apache.mina.transport.socket.DatagramSessionConfig;
 import org.apache.mina.transport.socket.SocketSessionConfig;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.socket.nio.NioDatagramChannel;
 import org.jboss.netty.channel.socket.nio.NioDatagramChannelFactory;
-import org.jboss.netty.channel.socket.nio.NioSocketChannel;
 
 import com.kaazing.mina.netty.ChannelIoSession;
-import com.kaazing.mina.netty.datagram.DatagramChannelIoAcceptor;
+import com.kaazing.mina.netty.datagram.DatagramChannelIoConnector;
 import com.kaazing.mina.netty.datagram.DatagramChannelIoSessionConfig;
 
-public class NioDatagramChannelIoAcceptor extends DatagramChannelIoAcceptor {
+public class NioDatagramChannelIoConnector extends DatagramChannelIoConnector {
 
     private static final TransportMetadata TRANSPORT_METADATA = new DefaultTransportMetadata(
-            "Kaazing", "NioDatagramChannel", false, true, InetSocketAddress.class,
-            DatagramSessionConfig.class, Object.class);
+            "Kaazing", "NioSocketChannel", false, true, InetSocketAddress.class,
+            SocketSessionConfig.class, Object.class);
 
-    public NioDatagramChannelIoAcceptor(DatagramChannelIoSessionConfig sessionConfig,
-            final NioDatagramChannelFactory channelFactory) {
+    public NioDatagramChannelIoConnector(DatagramChannelIoSessionConfig sessionConfig,
+            NioDatagramChannelFactory channelFactory) {
         super(sessionConfig, channelFactory);
     }
-
 
     @Override
     public TransportMetadata getTransportMetadata() {
