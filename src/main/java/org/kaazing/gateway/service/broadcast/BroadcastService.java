e0e27d431a3e82c8f098c408a593bfb37f27bb0b
Arthur Housinger <arthur.housinger@kaazing.com>
2014-11-17 07:46:41 +0000

    Migration to open source, initial commit

---
+++ src/main/java/org/kaazing/gateway/service/broadcast/BroadcastService.java
@@ -0,0 +1,232 @@
+/**
+ * Copyright (c) 2007-2014 Kaazing Corporation. All rights reserved.
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.kaazing.gateway.service.broadcast;
+
+// FIXME: Make InternalSystemProperty generic, maybe gateway.util?
+//import static org.kaazing.gateway.server.config.InternalSystemProperty.BROADCAST_SERVICE_DISCONNECT_CLIENTS_ON_RECONNECT;
+//import static org.kaazing.gateway.server.config.InternalSystemProperty.BROADCAST_SERVICE_MAXIMUM_PENDING_BYTES;
+import static org.kaazing.gateway.util.Utils.parseBoolean;
+import static org.kaazing.gateway.util.Utils.parsePositiveInteger;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Properties;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.annotation.Resource;
+
+import org.apache.mina.core.future.CloseFuture;
+import org.apache.mina.core.future.ConnectFuture;
+import org.apache.mina.core.future.IoFutureListener;
+import org.apache.mina.core.session.IoSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.kaazing.gateway.service.Service;
+import org.kaazing.gateway.service.ServiceContext;
+import org.kaazing.gateway.service.ServiceProperties;
+import org.kaazing.gateway.util.scheduler.SchedulerProvider;
+
+/**
+ * Gateway service of type "broadcast".
+ */
+public class BroadcastService implements Service {
+
+    private final Logger logger = LoggerFactory.getLogger("service.broadcast");
+    // FIXME: make this a logically named logger that does not look like a classname
+    private final Logger gatewayLogger = LoggerFactory.getLogger("org.kaazing.gateway.server.Gateway");
+
+    // FIXME: remove me
+    // services
+    private static final String BROADCAST_SERVICE_MAXIMUM_PENDING_BYTES = "org.kaazing.gateway.server.service.broadcast.MAXIMUM_PENDING_BYTES";
+    private static final String BROADCAST_SERVICE_DISCONNECT_CLIENTS_ON_RECONNECT = "org.kaazing.gateway.server.service.broadcast.DISCONNECT_CLIENTS_ON_RECONNECT"; // true or false
+    // FIXME: end of remove me
+
+    private ScheduledExecutorService scheduler;
+    private final AtomicBoolean reconnect;
+    private BroadcastServiceHandler handler;
+    private ServiceContext serviceContext;
+    private Properties configuration;
+
+    private URI acceptURI;
+    private URI connectURI;
+    private int reconnectDelay;
+
+    private final ConnectTask connectTask;
+
+    public BroadcastService() {
+        this.reconnect = new AtomicBoolean(false);
+        this.connectTask = new ConnectTask();
+    }
+
+    @Resource(name = "configuration")
+    public void setConfiguration(Properties configuration) {
+        this.configuration = configuration;
+    }
+
+    @Resource(name = "schedulerProvider")
+    public void setSchedulerProvider(SchedulerProvider provider) {
+        this.scheduler = provider.getScheduler("broadcast_reconnect", false);
+    }
+
+    @Override
+    public String getType() {
+        return "broadcast";
+    }
+
+    @Override
+    public void init(final ServiceContext serviceContext) throws Exception {
+        this.serviceContext = serviceContext;
+
+        boolean disconnectClientsOnReconnect = parseBoolean(
+                BROADCAST_SERVICE_DISCONNECT_CLIENTS_ON_RECONNECT,
+//                    BROADCAST_SERVICE_DISCONNECT_CLIENTS_ON_RECONNECT.getPropertyName(),
+                configuration.getProperty(BROADCAST_SERVICE_DISCONNECT_CLIENTS_ON_RECONNECT),
+//                    BROADCAST_SERVICE_DISCONNECT_CLIENTS_ON_RECONNECT.getProperty(configuration),
+                false);
+        long maximumScheduledWriteBytes = parsePositiveInteger(
+                BROADCAST_SERVICE_MAXIMUM_PENDING_BYTES,
+//                BROADCAST_SERVICE_MAXIMUM_PENDING_BYTES.getPropertyName(),
+                configuration.getProperty(BROADCAST_SERVICE_MAXIMUM_PENDING_BYTES),
+//                BROADCAST_SERVICE_MAXIMUM_PENDING_BYTES.getProperty(configuration),
+                Long.MAX_VALUE);
+        if ( maximumScheduledWriteBytes != Long.MAX_VALUE ) {
+            // The system property was specified
+            gatewayLogger.info(String.format("Broadcast service: limiting maximum scheduled write bytes to %d",
+                    maximumScheduledWriteBytes));
+        }
+        this.handler = new BroadcastServiceHandler(disconnectClientsOnReconnect, maximumScheduledWriteBytes,
+                serviceContext.getLogger());
+
+        Collection<URI> connectURIs = serviceContext.getConnects();
+        ServiceProperties properties = serviceContext.getProperties();
+        String accept = properties.get("accept");
+        String reconnectDelay = properties.get("reconnect.delay");
+        // TODO: change error message when connect property is documented
+        if (accept == null && (connectURIs == null || connectURIs.isEmpty())) {
+            throw new IllegalArgumentException("Missing required property: accept");
+        }
+
+        this.acceptURI = (accept != null) ? URI.create(accept) : null;
+        this.connectURI = (connectURIs == null || connectURIs.isEmpty()) ? null : connectURIs.iterator().next();
+        this.reconnectDelay = (reconnectDelay != null) ? Integer.parseInt(reconnectDelay) : 3000;
+    }
+
+    @Override
+    public void start() throws Exception {
+        reconnect.set(true);
+
+        serviceContext.bind(serviceContext.getAccepts(), handler);
+        serviceContext.bindConnectsIfNecessary(serviceContext.getConnects());
+
+        try {
+            if (acceptURI != null) {
+                serviceContext.bind(Arrays.asList(new URI[] { acceptURI }), handler.getListenHandler());
+            }
+
+            if (connectURI != null) {
+                scheduler.schedule(connectTask, 0, TimeUnit.MILLISECONDS);
+            }
+        } catch (Exception e) {
+            logger.error("Unable to bind to resource: " + acceptURI, e);
+        }
+    }
+
+    @Override
+    public void stop() throws Exception {
+        quiesce();
+
+        // defer until stop to allow connect to succeed and re-enable the service
+        serviceContext.unbindConnectsIfNecessary(serviceContext.getConnects());
+        
+        if (serviceContext != null) {
+            for (IoSession session : serviceContext.getActiveSessions()) {
+                session.close(true);
+            }
+        }
+
+        connectTask.stop();
+    }
+
+    @Override
+    public void quiesce() throws Exception {
+        reconnect.set(true);
+
+        if (serviceContext != null) {
+            serviceContext.unbind(serviceContext.getAccepts(), handler);
+
+            if (acceptURI != null) {
+                serviceContext.unbind(Arrays.asList(new URI[] { acceptURI }), handler.getListenHandler());
+            }
+        }
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        scheduler.shutdownNow();
+    }
+
+    private final class ConnectTask implements Runnable {
+        private final AtomicReference<IoSession> session;
+
+        public ConnectTask() {
+            session = new AtomicReference<IoSession>();
+        }
+
+        public void stop() {
+            IoSession connection = session.get();
+            if (connection != null && !connection.isClosing()) {
+                connection.close(true);
+            }
+        }
+
+        @Override
+        public void run() {
+            serviceContext.connect(connectURI, handler.getListenHandler(), null).addListener(new IoFutureListener<ConnectFuture>() {
+                @Override
+                public void operationComplete(ConnectFuture future) {
+                    if (future.isConnected()) {
+                        IoSession newSession = future.getSession();
+                        newSession.getCloseFuture().addListener(new IoFutureListener<CloseFuture>() {
+                            @Override
+                            public void operationComplete(CloseFuture future) {
+                                session.set(null);
+
+                                if (reconnect.get()) {
+                                    scheduler.schedule(connectTask, reconnectDelay, TimeUnit.MILLISECONDS);
+                                }
+                            }
+                        });
+                        session.set(newSession);
+                    }
+                    else {
+                        scheduler.schedule(connectTask, reconnectDelay, TimeUnit.MILLISECONDS);
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ src/test/java/org/kaazing/gateway/service/broadcast/BroadcastServiceTest.java
@@ -0,0 +1,478 @@
+/**
+ * Copyright (c) 2007-2014 Kaazing Corporation. All rights reserved.
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.kaazing.gateway.service.broadcast;
+
+import static java.lang.String.format;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.MethodRule;
+import org.kaazing.gateway.service.ServiceContext;
+import org.kaazing.gateway.service.ServiceFactory;
+import org.kaazing.gateway.service.ServiceProperties;
+import org.kaazing.gateway.util.scheduler.SchedulerProvider;
+import org.kaazing.test.util.MethodExecutionTrace;
+
+public class BroadcastServiceTest {
+    @Rule
+    public MethodRule testExecutionTrace = new MethodExecutionTrace();
+
+    private BroadcastService service;
+
+    @Before
+    public void setup() {
+        PropertyConfigurator.configure("src/test/resources/log4j-trace.properties");
+
+        service = (BroadcastService)ServiceFactory.newServiceFactory().newService("broadcast");
+        service.setSchedulerProvider(new SchedulerProvider());
+    }
+
+    @Test
+    public void testReconnect() throws Exception {
+        final ServiceContext serviceContext = new TestServiceContext(service, "test-broadcast", URI.create("tcp://localhost:9880"), URI.create("tcp://localhost:9897"));
+
+        // set up the configuration -- empty properties so the values just default
+        service.setConfiguration(new Properties());
+
+        TestBackendService backend = new TestBackendService();
+        Thread t = new Thread(backend, "BackendServiceThread");
+        t.start();
+
+        service.init(serviceContext);
+        service.start();
+
+        // wait for initial connection to back-end
+        CountDownLatch latch = backend.getLatch();
+        if (!latch.await(5, TimeUnit.SECONDS)) {
+            Assert.fail("Failed to establish connection to backend in 5 seconds");
+        }
+
+        // quit back-end
+        backend.stop();
+        t.join();
+
+        // restart back-end, wait for service to connect
+        backend = new TestBackendService();
+        t = new Thread(backend, "BackendServiceThread2");
+        t.start();
+
+        latch = backend.getLatch();
+        if (!latch.await(5, TimeUnit.SECONDS)) {
+            // Default reconnect is 3 seconds, so connection should get established within 3 seconds, make it 5 to be sure
+            Assert.fail("Failed to reconnect to backend in 5 seconds");
+        }
+
+        backend.stop();
+        t.join();
+
+        service.stop();
+        service.destroy();
+    }
+
+    @Test
+    public void testBroadcast() throws Exception {
+        final TestServiceContext serviceContext = new TestServiceContext(service, "test-broadcast", URI.create("tcp://localhost:9880"), null);
+        ServiceProperties serviceProperties = serviceContext.getProperties();
+        serviceProperties.put("accept", "tcp://localhost:9090");
+
+        // set up the configuration -- empty properties so the values just default
+        service.setConfiguration(new Properties());
+
+        service.init(serviceContext);
+        service.start();
+
+        TestBackendProducer producer = new TestBackendProducer();
+        Thread t = new Thread(producer, "BackendProducerThread");
+        t.start();
+
+        // wait for initial connection from producer
+        if (!producer.getLatch().await(5, TimeUnit.SECONDS)) {
+            Assert.fail("Failed to receive connection from producer in 5 seconds");
+        }
+
+        TestClient c1 = new TestClient(1);
+        TestClient c2 = new TestClient(2);
+        TestClient c3 = new TestClient(3);
+
+        Thread tc1 = new Thread(c1, "Client 1");
+        Thread tc2 = new Thread(c2, "Client 2");
+        Thread tc3 = new Thread(c3, "Client 3");
+
+        tc1.start();
+        tc2.start();
+        tc3.start();
+
+        if (!c1.getLatch().await(5, TimeUnit.SECONDS)) {
+            Assert.fail("Client 1 failed to receive message in 5 seconds");
+        }
+        if (!c2.getLatch().await(5, TimeUnit.SECONDS)) {
+            Assert.fail("Client 2 failed to receive message in 5 seconds");
+        }
+        if (!c3.getLatch().await(5, TimeUnit.SECONDS)) {
+            Assert.fail("Client 3 failed to receive message in 5 seconds");
+        }
+
+        c1.stop();
+        c2.stop();
+        c3.stop();
+
+        tc1.join();
+        tc2.join();
+        tc3.join();
+
+        // quit back-end
+        producer.stop();
+        t.join();
+
+        service.stop();
+        service.destroy();
+    }
+
+    @Test
+    public void testSlowConsumer() throws Exception {
+        final TestServiceContext serviceContext = new TestServiceContext(service, "test-broadcast", URI.create("tcp://localhost:9880"), null);
+        ServiceProperties serviceProperties = serviceContext.getProperties();
+        serviceProperties.put("accept", "tcp://localhost:9090");
+
+        // set up the configuration -- empty properties so the values just default
+        Properties slowConsumerProps = new Properties();
+        slowConsumerProps.setProperty("org.kaazing.gateway.server.service.broadcast.MAXIMUM_PENDING_BYTES", "40000"); // FIXME: why this number?
+        service.setConfiguration(slowConsumerProps);
+
+        service.init(serviceContext);
+        service.start();
+
+        FastTestBackendProducer producer = new FastTestBackendProducer();
+        Thread t = new Thread(producer, "FastBackendProducerThread");
+
+        SlowTestClient c1 = new SlowTestClient(1);
+        Thread tc1 = new Thread(c1, "SlowClient 1");
+
+        try {
+            t.start();
+
+            // wait for initial connection from producer
+            if (!producer.getLatch().await(5, TimeUnit.SECONDS)) {
+                Assert.fail("Failed to receive connection from producer in 5 seconds");
+            }
+
+            tc1.start();
+
+            if (!c1.getLatch().await(5, TimeUnit.SECONDS)) {
+                Assert.fail("SlowClient 1 failed to connect in 5 seconds");
+            }
+
+            // Wait up to 15 seconds for slow client to get killed, after that the test has timed out and failure is asserted
+            tc1.join(15 * 1000);
+            if (tc1.isAlive()) {
+                c1.stop();
+                tc1.join();
+                Assert.fail("SlowClient 1 was not terminated in 30 seconds");
+            }
+	} finally {
+            // quit back-end
+            producer.stop();
+            t.join();
+	}
+
+        service.stop();
+        service.destroy();
+    }
+
+    private class TestBackendService implements Runnable {
+        // a simple Backend that has a listener (ServerSocket) and has an API for producing a message
+        private ServerSocket s;
+        private boolean running = false;
+        private CountDownLatch latch = new CountDownLatch(1);
+
+        private TestBackendService() {
+            try {
+                s = new ServerSocket(9897);
+                running = true;
+            } catch (IOException ex) {
+                throw new RuntimeException("Failed to create TestBackendSerivce", ex);
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                Socket socket = s.accept();
+                OutputStream os = socket.getOutputStream();
+
+                latch.countDown(); // someone connected, count down
+                while (isRunning()) {
+                    // The bytes for ">|<"
+                    os.write(new byte[] { 0x3E, 0x7C, 0x3C});
+                    try {
+                        Thread.sleep(500); // sending 2 messages / second
+                    } catch (InterruptedException interEx) {
+                        // ignore
+                    }
+                }
+                socket.close();
+            } catch (IOException ex) {
+                throw new RuntimeException("Issue in TestBackendService.run()", ex);
+            }
+        }
+
+        private CountDownLatch getLatch() {
+            return latch;
+        }
+
+        private boolean isRunning() {
+            return running;
+        }
+
+        private void stop() {
+            running = false;
+            try {
+                s.close();
+            } catch (IOException ex) {
+                throw new RuntimeException("Failed to close ServerSocket", ex);
+            }
+        }
+    }
+
+    private class TestBackendProducer implements Runnable {
+        private CountDownLatch latch = new CountDownLatch(1);
+        private boolean running = true;
+
+        @Override
+        public void run() {
+            try {
+                Socket socket = new Socket();
+                socket.connect(new InetSocketAddress("localhost", 9090));
+                OutputStream os = socket.getOutputStream();
+
+                latch.countDown(); // someone connected, count down
+                while (running == true) {
+                    // The bytes for ">|<"
+                    os.write(new byte[] { 0x3E, 0x7C, 0x3C});
+                    try {
+                        Thread.sleep(500); // sending 2 messages / second
+                    } catch (InterruptedException interEx) {
+                        // ignore
+                    }
+                }
+                socket.close();
+            } catch (IOException ex) {
+                throw new RuntimeException("Issue in TestBackendProducer.run()", ex);
+            }
+        }
+
+        private void stop() {
+            running = false;
+        }
+
+        private CountDownLatch getLatch() {
+            return latch;
+        }
+    }
+
+    private class TestClient implements Runnable {
+        private CountDownLatch latch = new CountDownLatch(1);
+        private boolean running = true;
+        private int clientNumber;
+
+        private TestClient(int num) {
+            clientNumber = num;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Socket socket = new Socket();
+                socket.connect(new InetSocketAddress("localhost", 9880));
+                InputStream in = socket.getInputStream();
+
+                // read a message, countdown the latch, then keep reading messages until stop is called
+                byte[] b = new byte[3];
+                int numBytes = in.read(b);
+
+                if (numBytes == 3) {
+                    System.out.println(format("TestClient %d:  received message %s", clientNumber, new String(b)));
+                    latch.countDown(); // someone connected, count down
+                } else {
+                    System.out.println(format("Failure in TestClient %d:  read returned %d", clientNumber, numBytes));
+                }
+
+                while (running == true) {
+                    numBytes = in.read(b);
+                    if (numBytes < 0) {
+                        System.out.println(format("TestClient %d:  EOF, done reading, quitting client"));
+                        break;
+                    }
+
+                    if (numBytes == 3) {
+                        System.out.println(format("TestClient %d:  received message %s", clientNumber, new String(b)));
+                    } else {
+                        System.out.println(format("Failure in TestClient %d:  read returned %d", clientNumber, numBytes));
+                    }
+                }
+                socket.close();
+            } catch (IOException ex) {
+                throw new RuntimeException("Issue in TestClient.run()", ex);
+            }
+        }
+
+        private void stop() {
+            running = false;
+        }
+
+        private CountDownLatch getLatch() {
+            return latch;
+        }
+    }
+
+    private class FastTestBackendProducer implements Runnable {
+        private CountDownLatch latch = new CountDownLatch(1);
+        private boolean running = true;
+
+        @Override
+        public void run() {
+            try {
+                Socket socket = new Socket();
+                socket.connect(new InetSocketAddress("localhost", 9090));
+                OutputStream os = socket.getOutputStream();
+
+                // Some diagnostics for the test
+                System.out.println(format("FastTestBackendProducer receive buffer size: %d", socket.getReceiveBufferSize()));
+                System.out.println(format("FastTestBackendProducer send buffer size: %d", socket.getSendBufferSize()));
+
+                // The bytes for ">|<"
+                byte[] packet = new byte[] { 0x3E, 0x7C, 0x3C };
+                int messagesPerSecond = 20;
+                int testLengthTime = 4;
+                long targetBytes = 2 * (socket.getReceiveBufferSize() + socket.getSendBufferSize());
+                long batchSize = targetBytes / (messagesPerSecond * testLengthTime);
+                long totalBytesSent = 0;
+                long bytesSent = 0;
+
+                System.out.println(format("FastTestBackendProducer targetBytes: %d", targetBytes));
+                System.out.println(format("FastTestBackendProducer batch size: %d", batchSize));
+
+                long startTime = System.currentTimeMillis();
+                latch.countDown(); // someone connected, count down
+                while (running) {
+                    while (bytesSent < batchSize) {
+                        os.write(packet);
+                        bytesSent += packet.length;
+                    }
+
+                    totalBytesSent += bytesSent;
+                    bytesSent = 0;
+                    long delta = (System.currentTimeMillis() - startTime);
+
+                    // In the event the test fails, print some diagnostics
+                    System.out.println(format("Time: %d  Producer, sent %d bytes", delta, totalBytesSent));
+                    try {
+                        Thread.sleep(1000 / messagesPerSecond); // sending 20 messages / second
+                    } catch (InterruptedException interEx) {
+                        // ignore
+                    }
+                }
+                socket.close();
+            } catch (IOException ex) {
+                throw new RuntimeException("Issue in TestBackendProducer.run()", ex);
+            }
+        }
+
+        private void stop() {
+            running = false;
+        }
+
+        private CountDownLatch getLatch() {
+            return latch;
+        }
+    }
+
+    private class SlowTestClient implements Runnable {
+        private CountDownLatch latch = new CountDownLatch(1);
+        private int clientNumber;
+        private boolean stopped = false;
+
+        private SlowTestClient(int num) {
+            clientNumber = num;
+        }
+
+        public void stop() {
+            stopped = true;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Socket socket = new Socket();
+                socket.connect(new InetSocketAddress("localhost", 9880));
+                System.out.println(format("SlowTestClient %d: socket is %s", clientNumber, socket.toString()));
+                System.out.println(format("SlowTestClient receive buffer size: %d", socket.getReceiveBufferSize()));
+                System.out.println(format("SlowTestClient send buffer size: %d", socket.getSendBufferSize()));
+                InputStream in = socket.getInputStream();
+                OutputStream os = socket.getOutputStream();
+                latch.countDown(); // SlowTestClient successfully connected, now won't read
+
+                int iteration = 0;
+                while (!socket.isClosed() && socket.isConnected() && !socket.isInputShutdown()) {
+                    // take little naps, mmmmmmmmm naps....
+                    try {
+                        Thread.sleep(1000);
+                        System.out.println(format("SlowTestClient %d: iteration #%d has %d bytes available", clientNumber, iteration++, in.available()));
+                        try {
+                            os.write(new byte[] { 0x21 }); // write '!' back to the Gateway
+                        } catch (IOException writeIOEx) {
+                            // Expected as this is the way the socket is tested for being closed, just quit the loop
+                            break;
+                        }
+                    } catch (InterruptedException interEx) {
+                        System.out.println(format("SlowTestClient %d: someone woke me from my slumber!", clientNumber));
+                        break;
+                    }
+                }
+
+                System.out.println(format("SlowTestClient %d: connection closed, that'll teach me", clientNumber));
+
+                socket.close();
+            } catch (IOException ex) {
+                throw new RuntimeException("Issue in TestClient.run()", ex);
+            }
+        }
+
+        private CountDownLatch getLatch() {
+            return latch;
+        }
+    }
+}
