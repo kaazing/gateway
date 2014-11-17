cfe330a513ed9a00c37857265d074af55fe0c605
Arthur Housinger <arthur.housinger@kaazing.com>
2014-11-17 07:49:22 +0000

    Migration to open source, initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# gateway.transport.sse
+
+# About this Project
+
+The gateway.transport.sse is an implemenation of Server-Sent Events transport. 
+
+# Building this Project
+
+## Minimum requirements for building the project
+* Java SE Development Kit (JDK) 7 or higher
+
+## Steps for building this project
+0. Clone the repo
+0. mvn clean install
+
+# Running this Project
+
+0. Integrate this component in gateway.distribution by updating the version in gateway.distribution's pom
+0. Build the corresponding gateway.distribution and use it for application development
---
+++ src/main/java/org/kaazing/gateway/transport/sse/bridge/filter/SseBufferAllocator.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2007-2014 Kaazing Corporation. All rights reserved.
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.kaazing.gateway.transport.sse.bridge.filter;
+
+import java.nio.ByteBuffer;
+
+import org.kaazing.gateway.transport.sse.bridge.filter.SseBuffer.SseSharedBuffer;
+import org.kaazing.gateway.transport.sse.bridge.filter.SseBuffer.SseUnsharedBuffer;
+import org.kaazing.mina.core.buffer.AbstractIoBufferAllocatorEx;
+import org.kaazing.mina.core.buffer.IoBufferAllocatorEx;
+import org.kaazing.mina.core.buffer.IoBufferEx;
+
+public final class SseBufferAllocator extends AbstractIoBufferAllocatorEx<SseBuffer> {
+
+    private final IoBufferAllocatorEx<?> parent;
+
+    public SseBufferAllocator(IoBufferAllocatorEx<?> parent) {
+        this.parent = parent;
+    }
+    
+    @Override
+    public ByteBuffer allocate(int capacity, int flags) {
+        boolean offset = (flags & IoBufferEx.FLAG_ZERO_COPY) != IoBufferEx.FLAG_NONE;
+        if (offset) {
+            // data: [data] 0x0a
+            // 0x0a
+            // Note: each newline in [data] is replaced by 6 bytes of meta data
+            //       so zero-copy requires no newlines in data
+            int frameOffset = 5; 
+            int framePadding = 2;
+            ByteBuffer buf = parent.allocate(frameOffset + capacity + framePadding, flags);
+            buf.position(buf.position() + frameOffset);
+            buf.limit(buf.position() + capacity);
+            return buf;
+        }
+        else {
+            // no offset
+            return parent.allocate(capacity, flags);
+        }
+    }
+
+    @Override
+    public SseBuffer wrap(ByteBuffer nioBuffer, int flags) {
+        boolean shared = (flags & IoBufferEx.FLAG_SHARED) != IoBufferEx.FLAG_NONE;
+        return shared ? new SseSharedBuffer(nioBuffer) : new SseUnsharedBuffer(nioBuffer);
+    }
+
+}
