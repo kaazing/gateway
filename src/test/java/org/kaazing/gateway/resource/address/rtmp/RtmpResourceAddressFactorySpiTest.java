ed0d179abf1ab3c7de14f2b7635c66e893da972f
Arthur Housinger <arthur.housinger@kaazing.com>
2014-11-17 07:42:49 +0000

    Migration to open source, initial commit

---
+++ src/test/java/org/kaazing/gateway/resource/address/rtmp/RtmpResourceAddressFactorySpiTest.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2007-2014 Kaazing Corporation. All rights reserved.
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.kaazing.gateway.resource.address.rtmp;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.kaazing.gateway.resource.address.ResourceAddress.NEXT_PROTOCOL;
+import static org.kaazing.gateway.resource.address.ResourceAddress.QUALIFIER;
+import static org.kaazing.gateway.resource.address.ResourceAddress.TRANSPORT;
+import static org.kaazing.gateway.resource.address.ResourceAddress.TRANSPORT_URI;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.kaazing.gateway.resource.address.ResourceAddress;
+
+public class RtmpResourceAddressFactorySpiTest {
+
+    private RtmpResourceAddressFactorySpi addressFactorySpi;
+    private URI addressURI;
+    private Map<String, Object> options;
+    
+    @Before
+    public void before() {
+        addressFactorySpi = new RtmpResourceAddressFactorySpi();
+        addressURI = URI.create("rtmp://localhost:2020/");
+        options = new HashMap<String, Object>();
+        options.put("rtmp.nextProtocol", "custom");
+        options.put("rtmp.qualifier", "random");
+        options.put("rtmp.transport", URI.create("tcp://localhost:2121"));
+    }
+
+    @Test
+    public void shouldHaveRtmpSchemeName() throws Exception {
+        assertEquals("rtmp", addressFactorySpi.getSchemeName());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void shouldRequireRtmpSchemeName() throws Exception {
+        addressFactorySpi.newResourceAddress(URI.create("test://opaque"));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void shouldRequireExplicitPath() throws Exception {
+        addressFactorySpi.newResourceAddress(URI.create("rtmp://localhost:2020"));
+    }
+
+    @Test
+    public void shouldNotRequireExplicitPort() throws Exception {
+        ResourceAddress address = addressFactorySpi.newResourceAddress(URI.create("rtmp://localhost/"));
+        URI location = address.getResource();
+        assertEquals(location.getPort(), 1935);
+    }
+
+    @Test
+    public void shouldCreateAddressWithDefaultOptions() throws Exception {
+        ResourceAddress address = addressFactorySpi.newResourceAddress(addressURI);
+        assertNull(address.getOption(NEXT_PROTOCOL));
+        assertNull(address.getOption(QUALIFIER));
+        assertNull(address.getOption(TRANSPORT));
+    }
+
+    @Test
+    public void shouldCreateAddressWithOptions() {
+        ResourceAddress address = addressFactorySpi.newResourceAddress(addressURI, options);
+        assertEquals("custom", address.getOption(NEXT_PROTOCOL));
+        assertEquals("random", address.getOption(QUALIFIER));
+        assertNull(address.getOption(TRANSPORT));
+    }
+
+    @Test
+    public void shouldCreateAddressWithDefaultTransport() throws Exception {
+        ResourceAddress address = addressFactorySpi.newResourceAddress(addressURI);
+        assertNotNull(address.getOption(TRANSPORT_URI));
+        assertEquals(URI.create("tcp://localhost:2020"), address.getOption(TRANSPORT_URI));
+    }
+    
+    @Test
+    public void shouldCreateAddressWithTransport() throws Exception {
+        ResourceAddress address = addressFactorySpi.newResourceAddress(addressURI, options);
+        assertNotNull(address.getOption(TRANSPORT_URI));
+        assertEquals(URI.create("tcp://localhost:2121"), address.getOption(TRANSPORT_URI));
+    }
+    
+}
