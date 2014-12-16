40c8de3b6b1a605ee88e3d699c0b75d9c00d3756
David Witherspoon <david.witherspoon@kaazing.com>
2014-12-16 11:05:00 -0800

    Fixed case for when to use org vs com

--- src/main/java/org/kaazing/gateway/service/update/check/UpdateCheckService.java
+++ src/main/java/org/kaazing/gateway/service/update/check/UpdateCheckService.java
@@ -64,8 +64,8 @@ public class UpdateCheckService implements Service {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        versionServiceUrl = (productName.toLowerCase().contains("community")) ? "https://version.kaazing.org"
-                : "https://version.kaazing.com";
+        versionServiceUrl = (productName.toLowerCase().contains("enterprise")) ? "https://version.kaazing.com"
+                : "https://version.kaazing.org";
         // useful for local testing
         // versionServiceUrl = (productName.toLowerCase().contains("community")) ? "http://version.kaazing.org"
         // : "http://version.kaazing.com";
