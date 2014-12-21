70a9094fa237886da976725b75b589ff39674344
Steven Atkinson <steven@atkinson.net>
2014-12-21 01:50:02 -0800

    Apply kaazing checkstyle rules from code.quality tot he gateway.server.api project.  Lessons learned: * copyright header is not consistent with required code.quality, fixed to make comma optional * trailing spaces inside the license text at the top of each file should be fixed globally * many errors are simply trailing-whitespace lines; perl-pi.bak -e 's#(\s+)\n#\n#g' fixes most issues

--- src/main/java/org/kaazing/gateway/server/GatewayFactory.java
+++ src/main/java/org/kaazing/gateway/server/GatewayFactory.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2007-2014 Kaazing Corporation. All rights reserved.
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -8,9 +8,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -30,8 +30,11 @@ import org.kaazing.gateway.server.impl.GatewayCreator;
  * to accomplish this.
  * @see Gateway
  */
-public class GatewayFactory {
+public final class GatewayFactory {
     private static final ServiceLoader<GatewayCreator> loader = ServiceLoader.load(GatewayCreator.class);
+
+    private GatewayFactory() {
+    }
 
     /**
      * Creates an implementation of an Gateway.
--- src/main/java/org/kaazing/gateway/server/impl/VersionUtils.java
+++ src/main/java/org/kaazing/gateway/server/impl/VersionUtils.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2007-2014 Kaazing Corporation. All rights reserved.
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -8,9 +8,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -22,104 +22,107 @@
 package org.kaazing.gateway.server.impl;
 
 import java.io.IOException;
-import java.util.HashMap; 
-import java.util.HashSet; 
-import java.util.Iterator;
-import java.util.jar.Attributes; 
-import java.util.jar.JarFile; 
-import java.util.jar.Manifest; 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
 public final class VersionUtils {
 
-    public static String PRODUCT_TITLE = null;
-    public static String PRODUCT_VERSION = null;
-    public static String PRODUCT_EDITION = null;
-    public static String PRODUCT_DEPENDENCIES = null;
-    
+    public static String PRODUCT_TITLE;
+    public static String PRODUCT_VERSION;
+    public static String PRODUCT_EDITION;
+    public static String PRODUCT_DEPENDENCIES;
+
+    private VersionUtils() {
+    }
+
     public static String getGatewayProductTitle() {
-        getGatewayProductInfo();        
+        getGatewayProductInfo();
         return PRODUCT_TITLE;
     }
-    
+
     public static String getGatewayProductVersion() {
-        getGatewayProductInfo();        
+        getGatewayProductInfo();
         return PRODUCT_VERSION;
     }
-    
+
     public static String getGatewayProductVersionMajor() {
         String v = getGatewayProductVersion();
-        
+
         if (v == null) {
             return null;
         }
-        
+
         int dotPos = v.indexOf(".");
-        return (dotPos < 0 ? v : v.substring(0, dotPos));
+        return dotPos < 0 ? v : v.substring(0, dotPos);
     }
 
     public static String getGatewayProductVersionMinor() {
         String v = getGatewayProductVersion();
-        
+
         if (v == null || v.length() == 0) {
             return null;
         }
-        
+
         int dotPos = v.indexOf(".");
-        
+
         if (dotPos < 0) {
             return v + ".0";
         }
-        
+
         dotPos = v.indexOf(".", dotPos + 1);  // 2nd dot
-        
-        return (dotPos < 0 ? v : v.substring(0, dotPos));
+
+        return dotPos < 0 ? v : v.substring(0, dotPos);
     }
 
     public static String getGatewayProductVersionPatch() {
         String v = getGatewayProductVersion();
-        
+
         if (v == null || v.length() == 0) {
             return null;
         }
-        
+
         int dotPos = v.indexOf(".");
-        
+
         if (dotPos < 0) {
             return v + ".0.0";
         }
-        
+
         dotPos = v.indexOf(".", dotPos + 1);  // 2nd dot
-        
+
         if (dotPos < 0) {
             return v + ".0";
         }
-        
+
         dotPos = v.indexOf(".", dotPos + 1);  // 3rd dot
-        
-        return (dotPos < 0 ? v : v.substring(0, dotPos));
+
+        return dotPos < 0 ? v : v.substring(0, dotPos);
     }
 
     public static String getGatewayProductVersionBuild() {
         String v = getGatewayProductVersion();
-        
+
         if (v == null || v.length() == 0) {
             return null;
         }
-        
+
         int dotPos = v.indexOf(".");
-        
+
         if (dotPos < 0) {
             return v + ".0.0.0";
         }
-        
+
         dotPos = v.indexOf(".", dotPos + 1);  // 2nd dot
-        
+
         if (dotPos < 0) {
             return v + ".0.0";
         }
-        
+
         dotPos = v.indexOf(".", dotPos + 1);  // 3rd dot
-        
+
         if (dotPos < 0) {
             return v + ".0";
         }
@@ -129,48 +132,47 @@ public final class VersionUtils {
     }
 
     public static String getGatewayProductEdition() {
-        getGatewayProductInfo();                
+        getGatewayProductInfo();
         return PRODUCT_EDITION;
     }
 
     public static String getGatewayProductDependencies() {
-        getGatewayProductInfo();        
+        getGatewayProductInfo();
         return PRODUCT_DEPENDENCIES;
     }
 
     /**
      * Find the product information from the server JAR MANIFEST files and store it
      * in static variables here for later retrieval.
-     * 
-     * @return the product name
+     *
      */
-    private static final void getGatewayProductInfo() {
+    private static void getGatewayProductInfo() {
         // TODO: Now that we've switched the products to include
         // an "assembly.version" JAR, this routine could be greatly
         // simplified. Removals and dependencies should no longer be needed.
-        
+
         if (PRODUCT_TITLE != null) {
             // We've already run through this before, so do nothing.
             return;
         }
-        
+
         boolean foundJar = false;
 
         String[] pathEntries = System.getProperty("java.class.path").split(System.getProperty("path.separator"));
-        HashMap<String, Attributes> products = new HashMap<String, Attributes>(7);
-        HashSet<String> removals = new HashSet<String>(7);
+        HashMap<String, Attributes> products = new HashMap<>(7);
+        HashSet<String> removals = new HashSet<>(7);
         for (String pathEntry : pathEntries) {
             if (pathEntry.contains("distribution.version")) {
                 try {
                     JarFile jar = new JarFile(pathEntry);
                     Manifest mf = jar.getManifest();
                     Attributes attrs = mf.getMainAttributes();
-                    if ( attrs != null ) {
+                    if (attrs != null) {
                         String title = attrs.getValue("Implementation-Title");
                         String version = attrs.getValue("Implementation-Version");
                         String product = attrs.getValue("Kaazing-Product");
                         String dependencies = attrs.getValue("Kaazing-Dependencies");
-                        if ( title != null && version != null ) {
+                        if (title != null && version != null) {
                             foundJar = true;
 
                             // Store the list of products found, but remove any products
@@ -178,23 +180,22 @@ public final class VersionUtils {
                             // product depends.  We want to find the product that nothing
                             // else depends on.
                             products.put(product != null ? product : title, attrs);
-                            if ( dependencies != null ) {
+                            if (dependencies != null) {
                                 String[] deps = dependencies.split(",");
-                                for (String dep : deps) {
-                                    removals.add(dep);
-                                }
+                                Collections.addAll(removals, deps);
                             }
                         }
                     }
                 }
                 catch (IOException e) {
+                    // ignore
                 }
             }
         }
 
         // remove any products that depend on other products
-        for (Iterator<String> it = removals.iterator(); it.hasNext(); ) {
-            products.remove(it.next());
+        for (String removal : removals) {
+            products.remove(removal);
         }
 
         if (!foundJar || products.size() == 0) {
