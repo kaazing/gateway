98122ffabcf643dcaa72a2a8f391b951d5db489d
Chris Barrow <chris.barrow@kaazing.com>
2015-03-17 16:42:27 -0700

    When Upgrade header is not present (usually due to proxy not forwarding them), the filter looks at Sec-WebSocket-Key to populate next protocol to be websocket. This will enable the request to go websocket layer and fail there.

--- transport.wsn/src/test/java/org/kaazing/gateway/transport/wsn/handshake/WsHandshakeIT.java
+++ transport.wsn/src/test/java/org/kaazing/gateway/transport/wsn/handshake/WsHandshakeIT.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2007-2014 Kaazing Corporation. All rights reserved.
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -8,9 +8,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -21,13 +21,16 @@
 
 package org.kaazing.gateway.transport.wsn.handshake;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.rules.RuleChain.outerRule;
 
 import java.net.URI;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.DisableOnDebug;
 import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
 import org.kaazing.gateway.server.test.GatewayRule;
 import org.kaazing.gateway.server.test.config.GatewayConfiguration;
 import org.kaazing.gateway.server.test.config.builder.GatewayConfigurationBuilder;
@@ -37,6 +40,8 @@ import org.kaazing.k3po.junit.rules.K3poRule;
 public class WsHandshakeIT {
 
     private K3poRule robot = new K3poRule();
+
+    private TestRule timeout = new DisableOnDebug(new Timeout(5, SECONDS));
 
     private GatewayRule gateway = new GatewayRule() {
         {
@@ -50,6 +55,17 @@ public class WsHandshakeIT {
                                 .allowOrigin("http://localhost:8000")
                             .done()
                         .done()
+                        .service()
+                            .name("jms")
+                            .accept(URI.create("ws://localhost:8005/jms"))
+                            .accept(URI.create("wss://foo.example.com:443/jms"))
+                            .type("echo")
+                            .acceptOption("ssl.encryption", "disabled")
+                            .acceptOption("wss.bind", "8011")
+                            .crossOrigin()
+                                .allowOrigin("*")
+                            .done()
+                        .done()
                     .done();
             // @formatter:on
             init(configuration);
@@ -57,11 +73,17 @@ public class WsHandshakeIT {
     };
 
     @Rule
-    public TestRule chain = outerRule(robot).around(gateway);
+    public TestRule chain = outerRule(robot).around(gateway).around(timeout);
 
     @Specification("websocket.handshake")
     @Test
     public void shouldNegotiateKaazingHandshakeSubProtocol() throws Exception {
+        robot.finish();
+    }
+
+    @Specification("websocket.handshake.missing.upgrade")
+    @Test
+    public void websocketHandshakeMissingUpgrade() throws Exception {
         robot.finish();
     }
 }
