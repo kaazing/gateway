adc9281049fc2320be3349f29dcb11eb8c2e587b
Chris Barrow <chris.barrow@kaazing.com>
2015-03-17 10:09:12 -0700

    #59 Use a non-ephemeral port number in transport.wsn UnresolveableHostnameIT (21111 instead of 51111) to try to avoid sporadic failure due to port in use.

--- transport.wsn/src/test/java/org/kaazing/gateway/transport/wsn/UnresolvableHostnameIT.java
+++ transport.wsn/src/test/java/org/kaazing/gateway/transport/wsn/UnresolvableHostnameIT.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2007-2014 Kaazing Corporation. All rights reserved.
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -8,9 +8,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -55,7 +55,7 @@ public class UnresolvableHostnameIT {
                     .service()
                         .accept(URI.create("ws://unresolvable.kaazing.me:8000"))
                         .type("echo")
-                        .acceptOption("ws.bind", "51111")
+                        .acceptOption("ws.bind", "21111")
                     .done()
                 .done();
             init(configuration);
@@ -64,7 +64,7 @@ public class UnresolvableHostnameIT {
 
     @Rule
     public TestRule chain = outerRule(robot).around(gateway);
-    
+
     @Specification("connectingToUnresolvableHostnameWithBind")
     @Test(timeout = 2000)
     // Test case for KG-5301
