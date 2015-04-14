98c1ecd80b4b5b2313c5f3a9638fd40ff0ef1667
Jitendra Kotamraju <jitsni@gmail.com>
2015-04-14 12:32:37 -0700

    Setting up the upgrade handlers via CloseFuture of accept and connect sessions.

--- service/http.proxy/src/test/java/org/kaazing/gateway/service/http/proxy/HttpProxyUpgradeIT.java
+++ service/http.proxy/src/test/java/org/kaazing/gateway/service/http/proxy/HttpProxyUpgradeIT.java
@@ -23,7 +23,9 @@ package org.kaazing.gateway.service.http.proxy;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.DisableOnDebug;
 import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
 import org.kaazing.gateway.server.test.GatewayRule;
 import org.kaazing.gateway.server.test.config.GatewayConfiguration;
 import org.kaazing.gateway.server.test.config.builder.GatewayConfigurationBuilder;
@@ -32,9 +34,13 @@ import org.kaazing.k3po.junit.rules.K3poRule;
 
 import java.net.URI;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.rules.RuleChain.outerRule;
 
 public class HttpProxyUpgradeIT {
+
+    @Rule
+    public TestRule timeout = new DisableOnDebug(new Timeout(10, SECONDS));
 
     private final K3poRule robot = new K3poRule();
 
@@ -57,8 +63,8 @@ public class HttpProxyUpgradeIT {
     @Rule
     public TestRule chain = outerRule(robot).around(gateway);
 
-    @Specification( "http.proxy.upgrade.websocket")
-    @Test(timeout = 5000)
+    @Specification("http.proxy.upgrade.websocket")
+    @Test
     public void upgradeWebSocket() throws Exception {
         robot.finish();
     }
