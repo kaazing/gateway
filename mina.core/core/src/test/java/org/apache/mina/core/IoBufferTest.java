6e35a8fd10e5d33c4e1b3716a5a35364ce11e2bb
Jitendra Kotamraju <jitsni@gmail.com>
2015-04-24 18:32:40 -0700

    Removing unnecessary modifiers

--- mina.core/core/src/test/java/org/apache/mina/core/IoBufferTest.java
+++ mina.core/core/src/test/java/org/apache/mina/core/IoBufferTest.java
@@ -812,11 +812,11 @@ public class IoBufferTest {
     }
 
     // We need an enum with 64 values
-    private static enum TestEnum {
+    private enum TestEnum {
         E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, E11, E12, E13, E14, E15, E16, E17, E18, E19, E20, E21, E22, E23, E24, E25, E26, E27, E28, E29, E30, E31, E32, E33, E34, E35, E36, E37, E38, E39, E40, E41, E42, E43, E44, E45, E46, E77, E48, E49, E50, E51, E52, E53, E54, E55, E56, E57, E58, E59, E60, E61, E62, E63, E64
     }
 
-    private static enum TooBigEnum {
+    private enum TooBigEnum {
         E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, E11, E12, E13, E14, E15, E16, E17, E18, E19, E20, E21, E22, E23, E24, E25, E26, E27, E28, E29, E30, E31, E32, E33, E34, E35, E36, E37, E38, E39, E40, E41, E42, E43, E44, E45, E46, E77, E48, E49, E50, E51, E52, E53, E54, E55, E56, E57, E58, E59, E60, E61, E62, E63, E64, E65
     }
 
