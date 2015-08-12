d777716b7b39d07b30b7720d3d271626e9c3a5bd
Jitendra Kotamraju <jitsni@gmail.com>
2015-08-12 12:40:00 -0700

    Adding WsnAcceptorRule so that wsn spec tests can use it.

--- transport/wsn/src/test/java/org/kaazing/gateway/transport/wsn/specification/ws/FragmentationIT.java
+++ transport/wsn/src/test/java/org/kaazing/gateway/transport/wsn/specification/ws/FragmentationIT.java
@@ -17,8 +17,6 @@
 package org.kaazing.gateway.transport.wsn.specification.ws;
 
 import org.apache.mina.core.filterchain.IoFilterChain;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
@@ -26,20 +24,10 @@ import org.junit.rules.DisableOnDebug;
 import org.junit.rules.TestRule;
 import org.junit.rules.Timeout;
 import org.kaazing.gateway.resource.address.ResourceAddress;
-import org.kaazing.gateway.resource.address.ResourceAddressFactory;
-import org.kaazing.gateway.transport.BridgeServiceFactory;
 import org.kaazing.gateway.transport.IoHandlerAdapter;
-import org.kaazing.gateway.transport.TransportFactory;
-import org.kaazing.gateway.transport.http.HttpAcceptor;
-import org.kaazing.gateway.transport.http.HttpConnector;
-import org.kaazing.gateway.transport.nio.internal.NioSocketAcceptor;
-import org.kaazing.gateway.transport.nio.internal.NioSocketConnector;
-import org.kaazing.gateway.transport.ws.WsAcceptor;
 import org.kaazing.gateway.transport.ws.bridge.filter.WsBuffer;
-import org.kaazing.gateway.transport.ws.extension.WebSocketExtensionFactory;
-import org.kaazing.gateway.transport.wsn.WsnAcceptor;
+import org.kaazing.gateway.transport.wsn.WsnAcceptorRule;
 import org.kaazing.gateway.transport.wsn.WsnSession;
-import org.kaazing.gateway.util.scheduler.SchedulerProvider;
 import org.kaazing.k3po.junit.annotation.Specification;
 import org.kaazing.k3po.junit.rules.K3poRule;
 import org.kaazing.mina.core.buffer.IoBufferAllocatorEx;
@@ -47,12 +35,9 @@ import org.kaazing.mina.core.buffer.IoBufferEx;
 import org.kaazing.mina.core.session.IoSessionEx;
 import org.kaazing.test.util.MethodExecutionTrace;
 
-import java.net.URI;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.rules.RuleChain.outerRule;
@@ -64,97 +49,16 @@ import static org.kaazing.gateway.resource.address.ws.WsResourceAddress.LIGHTWEI
 public class FragmentationIT {
 
     private final K3poRule k3po = new K3poRule().setScriptRoot("org/kaazing/specification/ws/fragmentation");
+
+    private final WsnAcceptorRule acceptor = new WsnAcceptorRule();
+
+    private final TestRule testExecutionTrace = new MethodExecutionTrace();
     
-    
-    public TestRule testExecutionTrace = new MethodExecutionTrace();
-    
-    public final TestRule timeout = new DisableOnDebug(new Timeout(5, SECONDS));
+    private final TestRule timeout = new DisableOnDebug(new Timeout(5, SECONDS));
 
     @Rule
-    public final TestRule chain = outerRule(k3po).around(timeout).around(testExecutionTrace);
+    public final TestRule chain = outerRule(testExecutionTrace).around(acceptor).around(k3po).around(timeout);
 
-    private SchedulerProvider schedulerProvider;
-
-    private static int NETWORK_OPERATION_WAIT_SECS = 10; // was 3, increasing for loaded environments
-
-
-    private ResourceAddressFactory addressFactory;
-    private BridgeServiceFactory serviceFactory;
-
-    private NioSocketConnector tcpConnector;
-    private HttpConnector httpConnector;
-    
-    private NioSocketAcceptor tcpAcceptor;
-    private HttpAcceptor httpAcceptor;
-    private WsnAcceptor wsnAcceptor;
-    private WsAcceptor wsAcceptor;
-
-    private static final boolean DEBUG = false;
-
-    
-    @Before
-    public void init() {
-        schedulerProvider = new SchedulerProvider();
-        
-        addressFactory = ResourceAddressFactory.newResourceAddressFactory();
-        Map<String, Object> config = Collections.emptyMap();
-        TransportFactory transportFactory = TransportFactory.newTransportFactory(config);
-        serviceFactory = new BridgeServiceFactory(transportFactory);
-
-        tcpAcceptor = (NioSocketAcceptor)transportFactory.getTransport("tcp").getAcceptor();
-        tcpAcceptor.setResourceAddressFactory(addressFactory);
-        tcpAcceptor.setBridgeServiceFactory(serviceFactory);
-        tcpAcceptor.setSchedulerProvider(schedulerProvider);
-        
-        tcpConnector = (NioSocketConnector)transportFactory.getTransport("tcp").getConnector();
-        tcpConnector.setResourceAddressFactory(addressFactory);
-        tcpConnector.setBridgeServiceFactory(serviceFactory);
-        
-        httpAcceptor = (HttpAcceptor)transportFactory.getTransport("http").getAcceptor();
-        httpAcceptor.setBridgeServiceFactory(serviceFactory);
-        httpAcceptor.setResourceAddressFactory(addressFactory);
-        httpAcceptor.setSchedulerProvider(schedulerProvider);
-        
-        httpConnector = (HttpConnector)transportFactory.getTransport("http").getConnector();
-        httpConnector.setBridgeServiceFactory(serviceFactory);
-        httpConnector.setResourceAddressFactory(addressFactory);
-
-        wsnAcceptor = (WsnAcceptor)transportFactory.getTransport("wsn").getAcceptor();
-        wsnAcceptor.setBridgeServiceFactory(serviceFactory);
-        wsnAcceptor.setResourceAddressFactory(addressFactory);
-        wsnAcceptor.setSchedulerProvider(schedulerProvider);
-        wsAcceptor = new WsAcceptor(WebSocketExtensionFactory.newInstance());
-        wsnAcceptor.setWsAcceptor(wsAcceptor);
-
-    }
-
-    @After
-    public void disposeConnector() {
-        if (tcpAcceptor != null) {
-            tcpAcceptor.dispose();
-        }
-        if (httpAcceptor != null) {
-            httpAcceptor.dispose();
-        }
-        if (wsnAcceptor != null) {
-            wsnAcceptor.dispose();
-        }
-        if (tcpConnector != null) {
-            tcpConnector.dispose();
-        }
-        if (httpConnector != null) {
-            httpConnector.dispose();
-        }
-    }
-    
-    private void bindTo8080(IoHandlerAdapter adapter) {
-        final URI location = URI.create("ws://localhost:8080/echo");
-        Map<String, Object> addressOptions = Collections.emptyMap();
-        ResourceAddress address = addressFactory.newResourceAddress(location, addressOptions);
-        
-        wsnAcceptor.bind(address, adapter, null);
-    }
-    
 
     @Test
     @Specification({
@@ -162,8 +66,7 @@ public class FragmentationIT {
         })
     public void shouldFailWebSocketConnectionWhenClientSendContinuationFrameWithPayloadNotFragmented() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.CONTINUATION);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -173,8 +76,7 @@ public class FragmentationIT {
         })
     public void shouldFailWebSocketConnectionWhenClientSendContinuationFrameWithPayloadFragmented() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.CONTINUATION);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -184,8 +86,7 @@ public class FragmentationIT {
         })
     public void shouldEchoClientSendTextFrameWithPayloadNotFragmented() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.TEXT);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -196,9 +97,7 @@ public class FragmentationIT {
         })
     public void shouldEchoClientSendTextFrameWithEmptyPayloadFragmented() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.TEXT);
-        
-        bindTo8080(acceptHandler);
-        
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -209,8 +108,7 @@ public class FragmentationIT {
         })
     public void shouldEchoClientSendTextFrameWithEmptyPayloadFragmentedAndInjectedPingPong() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.TEXT);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -220,8 +118,7 @@ public class FragmentationIT {
         })
     public void shouldEchoClientSendTextFrameWithPayloadFragmented() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.TEXT);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -231,8 +128,7 @@ public class FragmentationIT {
         })
     public void shouldEchoClientSendTextFrameWithPayloadFragmentedWithSomeEmptyFragments() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.TEXT);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -243,8 +139,7 @@ public class FragmentationIT {
         })
     public void shouldEchoClientSendTextFrameWithPayloadFragmentedEvenWhenNotUTF8Aligned() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.TEXT);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -254,8 +149,7 @@ public class FragmentationIT {
         })
     public void shouldEchoClientSendTextFrameWithPayloadFragmentedAndInjectedPingPong() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.TEXT);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -265,8 +159,7 @@ public class FragmentationIT {
         })
     public void shouldFailWebSocketConnectionWhenClientSendTextFrameWithPayloadFragmentedButNotContinued() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.TEXT);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -276,8 +169,7 @@ public class FragmentationIT {
         })
     public void shouldEchoClientSendBinaryFrameWithPayloadNotFragmented() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.BINARY);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -288,8 +180,7 @@ public class FragmentationIT {
         })
     public void shouldEchoClientSendBinaryFrameWithEmptyPayloadFragmented() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.BINARY);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -300,8 +191,7 @@ public class FragmentationIT {
         })
     public void shouldEchoClientSendBinaryFrameWithEmptyPayloadFragmentedAndInjectedPingPong() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.BINARY);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -311,8 +201,7 @@ public class FragmentationIT {
         })
     public void shouldEchoClientSendBinaryFrameWithPayloadFragmented() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.BINARY);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -322,8 +211,7 @@ public class FragmentationIT {
         })
     public void shouldEchoClientSendBinaryFrameWithPayloadFragmentedWithSomeEmptyFragments() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.BINARY);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -333,8 +221,7 @@ public class FragmentationIT {
         })
     public void shouldEchoClientSendBinaryFrameWithPayloadFragmentedAndInjectedPingPong() throws Exception {
         IoHandlerAdapter<IoSessionEx> acceptHandler = textFragmentIoHandlerAdapter(WsBuffer.Kind.BINARY);
-        
-        bindTo8080(acceptHandler);
+        acceptor.bind("ws://localhost:8080/echo", acceptHandler);
         k3po.finish();
     }
 
@@ -343,7 +230,7 @@ public class FragmentationIT {
         "client.send.binary.payload.length.125.fragmented.but.not.continued/handshake.request.and.frames"
         })
     public void shouldFailWebSocketConnectionWhenClientSendBinaryFrameWithPayloadFragmentedButNotContinued() throws Exception {
-        bindTo8080(new IoHandlerAdapter());
+        acceptor.bind("ws://localhost:8080/echo", new IoHandlerAdapter());
         k3po.finish();
     }
 
@@ -352,7 +239,7 @@ public class FragmentationIT {
         "client.send.close.payload.length.2.fragmented/handshake.request.and.frames"
         })
     public void shouldFailWebSocketConnectionWhenClientSendCloseFrameWithPayloadFragmented() throws Exception {
-        bindTo8080(new IoHandlerAdapter());
+        acceptor.bind("ws://localhost:8080/echo", new IoHandlerAdapter());
         k3po.finish();
     }
 
@@ -361,7 +248,7 @@ public class FragmentationIT {
         "client.send.ping.payload.length.0.fragmented/handshake.request.and.frames"
         })
     public void shouldFailWebSocketConnectionWhenClientSendPingFrameWithPayloadFragmented() throws Exception {
-        bindTo8080(new IoHandlerAdapter());
+        acceptor.bind("ws://localhost:8080/echo", new IoHandlerAdapter());
         k3po.finish();
     }
 
@@ -370,7 +257,7 @@ public class FragmentationIT {
         "client.send.pong.payload.length.0.fragmented/handshake.request.and.frames"
         })
     public void shouldFailWebSocketConnectionWhenClientSendPongFrameWithPayloadFragmented() throws Exception {
-        bindTo8080(new IoHandlerAdapter());
+        acceptor.bind("ws://localhost:8080/echo", new IoHandlerAdapter());
         k3po.finish();
     }
     
@@ -380,10 +267,6 @@ public class FragmentationIT {
     private IoHandlerAdapter<IoSessionEx> textFragmentIoHandlerAdapter(WsBuffer.Kind kind) {
         IoHandlerAdapter<IoSessionEx> acceptHandler = new IoHandlerAdapter<IoSessionEx>() {
             List<ByteBuffer> bufferList = new ArrayList<ByteBuffer>();
-            @Override
-            protected void doSessionOpened(IoSessionEx session) throws Exception {
-                // custom logic
-            }
             
             @Override
             protected void doMessageReceived(IoSessionEx session, Object message) throws Exception {
