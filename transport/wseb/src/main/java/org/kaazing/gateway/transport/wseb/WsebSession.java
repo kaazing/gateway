aeac67a1146cbaa33f588278e06373b8e437a2cd
Jitendra Kotamraju <jitsni@gmail.com>
2015-09-22 11:01:22 -0700

    Removing println

--- transport/wseb/src/main/java/org/kaazing/gateway/transport/wseb/WsebSession.java
+++ transport/wseb/src/main/java/org/kaazing/gateway/transport/wseb/WsebSession.java
@@ -359,11 +359,7 @@ public class WsebSession extends AbstractWsBridgeSession<WsebSession, WsBuffer> 
     public void detachPendingWriter() {
         HttpSession pendingWriter = pendingNewWriter.getAndSet(null);
         if (pendingWriter != null) {
-            if (pendingWriter instanceof  HttpAcceptSession) {
-                ((HttpAcceptSession) pendingWriter).setStatus(HttpStatus.CLIENT_NOT_FOUND);
-            }
             if (LOGGER.isDebugEnabled()) {
-System.out.println(String.format("detachPendingWriter on WsebSession wseb#%d, writer %s", this.getId(), pendingWriter));
                 LOGGER.debug(String.format("detachPendingWriter on WsebSession wseb#%d, writer %s", this.getId(), pendingWriter));
             }
             pendingWriter.close(false);
