d587ae31a2f94c2dc10039d92b379ed22412ebbc
Irina Mitrea <imitrea@luxoft.com>
2015-09-29 20:08:12 +0300

    Added new idle timeout k3po tests

---
+++ transport/wsn/src/test/java/org/kaazing/gateway/transport/wsn/extensions/idletimeout/IdleTimeoutExtensionK3POIT.java
@@ -0,0 +1,110 @@
+/**
+ * Copyright (c) 2007-2014 Kaazing Corporation. All rights reserved.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.kaazing.gateway.transport.wsn.extensions.idletimeout;
+
+import static org.kaazing.test.util.ITUtil.createRuleChain;
+
+import java.net.URI;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.kaazing.gateway.server.test.GatewayRule;
+import org.kaazing.gateway.server.test.config.GatewayConfiguration;
+import org.kaazing.gateway.server.test.config.builder.GatewayConfigurationBuilder;
+import org.kaazing.k3po.junit.annotation.Specification;
+import org.kaazing.k3po.junit.rules.K3poRule;
+// Note: further testing of this extension is done in WsnInactivityTimeoutIT
+public class IdleTimeoutExtensionK3POIT {
+
+    private final K3poRule robot = new K3poRule().setScriptRoot("org/kaazing/specification/ws.extensions/x-kaazing-idle-timeout");
+
+    public GatewayRule gateway = new GatewayRule() {
+        {
+            GatewayConfiguration configuration = new GatewayConfigurationBuilder()
+                    .service()
+                        .accept(URI.create("wsn://localhost:8001/echo"))
+                        .type("echo")
+                        .crossOrigin()
+                            .allowOrigin("*")
+                        .done()
+                        .acceptOption("ws.inactivity.timeout", "2sec")
+                    .done()
+                    .service()
+                        .accept(URI.create("wsn://localhost:8001/echoZeroTimeout"))
+                        .type("echo")
+                        .crossOrigin()
+                            .allowOrigin("*")
+                        .done()
+                        .acceptOption("ws.inactivity.timeout", "0sec")
+                    .done()
+                .done();
+
+            init(configuration);
+        }
+    };
+
+    @Rule
+    public TestRule chain = createRuleChain(gateway, robot);
+
+    @Specification("downstream.data.sent.by.server.no.client.timeout/request")
+    @Test
+    public void downstreamDataSentByServerNoClientTimeout() throws Exception {
+        robot.finish();
+    }
+
+    @Specification("extension.ping.pong.should.not.be.native.client.closes.conn/request")
+    @Test
+    public void extensionPingPongEnabledShouldNotBeNativeClientClosesConnection() throws Exception {
+        robot.finish();
+    }
+
+    @Specification("extension.ping.pong.frames.sent.by.server.no.client.timeout/request")
+    @Test
+    public void extensionPingPongFramesSentByServerNoClientTimeout() throws Exception {
+        robot.finish();
+    }
+
+    @Specification("negative.timeout.sent.by.server.client.closes.connection/request")
+    @Test
+    public void negativeTimeoutSentByServerClientClosesConnection() throws Exception {
+        robot.finish();
+    }
+
+    @Specification("no.data.sent.by.server.client.timeout/request")
+    @Test
+    public void noDataSentByServerClientTimeout() throws Exception {
+        robot.finish();
+    }
+
+    @Specification("standard.ping.pong.frames.sent.by.server.no.client.timeout/request")
+    @Test
+    public void standardPingPongFramesSentByServerNoClientTimeout() throws Exception {
+        robot.finish();
+    }
+
+    @Specification("zero.timeout.sent.by.server.client.closes.connection/request")
+    @Test
+    public void zeroTimeoutSentByServerClientClosesConnection() throws Exception {
+        robot.finish();
+    }
+}
