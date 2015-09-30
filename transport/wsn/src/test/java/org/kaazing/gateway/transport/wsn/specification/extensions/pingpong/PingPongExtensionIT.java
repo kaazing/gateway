d9b6076db55446e46487a9ff147078eb3ec8aafc
Marina Gherghe <mgherghe@luxoft.com>
2015-09-30 16:40:52 +0300

    Added PingPongExtensionIT to run the WSN ping-pong tests from k3po

---
+++ transport/wsn/src/test/java/org/kaazing/gateway/transport/wsn/specification/extensions/pingpong/PingPongExtensionIT.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2014, Kaazing Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kaazing.gateway.transport.wsn.specification.extensions.pingpong;
+
+import static org.kaazing.test.util.ITUtil.createRuleChain;
+
+import java.net.URI;
+
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.kaazing.gateway.server.test.GatewayRule;
+import org.kaazing.gateway.server.test.config.GatewayConfiguration;
+import org.kaazing.gateway.server.test.config.builder.GatewayConfigurationBuilder;
+import org.kaazing.k3po.junit.annotation.Specification;
+import org.kaazing.k3po.junit.rules.K3poRule;
+
+
+public class PingPongExtensionIT {
+
+    private final K3poRule k3po = new K3poRule().setScriptRoot("org/kaazing/specification/ws.extensions/x-kaazing-ping-pong/wsn");
+
+    private GatewayRule gateway = new GatewayRule() {
+        {
+            GatewayConfiguration configuration = new GatewayConfigurationBuilder()
+                .service()
+                    .accept(URI.create("wsn://localhost:8001/echo"))
+                    .type("echo")
+                    .crossOrigin()
+                        .allowOrigin("*")
+                    .done()
+                    .acceptOption("ws.inactivity.timeout", "4secs")
+                .done()
+            .done();
+            init(configuration);
+        }
+    };
+
+    @Rule
+    public TestRule chain = createRuleChain(gateway, k3po);
+
+    @Test
+    @Specification({"server.should.reply.to.standard.ping.with.standard.pong/request" })
+    public void serverShouldReplyToStandardPingWithStandardPong() throws Exception {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({"server.should.reply.to.extended.ping.with.extended.pong/request" })
+    public void serverShouldReplyToExtendedPingWithExtendedPong() throws Exception {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({"server.may.send.extended.ping.control.frames/request" })
+    public void serverMaySendExtendedPingControlFrames() throws Exception {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({"client.should.reply.to.extended.ping.with.extended.pong/request" })
+    public void clientShouldReplyToExtendedPingWithExtendedPong() throws Exception {
+        k3po.finish();
+    }
+
+    @Test
+    @Ignore ("Not working yet")
+    @Specification({"client.should.reply.to.standard.ping.with.standard.pong/request" })
+    public void clientShouldReplyToStandardPingWithStandardPong() throws Exception {
+        k3po.finish();
+    }
+
+    @Test
+    @Ignore ("Not working yet")
+    @Specification({"client.should.disconnect.if.wrong.control.bytes.length/request" })
+    public void clientShouldDisconnectIfWrongControlBytesLength() throws Exception {
+        k3po.finish();
+    }
+
+    @Test
+    @Ignore ("Not working yet")
+    @Specification({"client.should.disconnect.if.wrong.control.bytes.value/request" })
+    public void clientShouldDisconnectIfWrongControlBytesValue() throws Exception {
+        k3po.finish();
+    }
+
+    @Test
+    @Ignore ("Not working yet")
+    @Specification({"client.should.disconnect.if.no.control.bytes.sent/request" })
+    public void clientShouldDisconnectIfNoControlBytesSent() throws Exception {
+        k3po.finish();
+    }
+}
