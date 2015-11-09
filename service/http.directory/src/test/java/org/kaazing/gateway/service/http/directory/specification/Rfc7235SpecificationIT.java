6652c2116f68e30f497b671553081bdb3ad97dee
Jitendra Kotamraju <jitsni@gmail.com>
2015-11-09 13:39:15 -0800

    Reworking the pull request #253

--- service/http.directory/src/test/java/org/kaazing/gateway/service/http/directory/specification/Rfc7235SpecificationIT.java
+++ service/http.directory/src/test/java/org/kaazing/gateway/service/http/directory/specification/Rfc7235SpecificationIT.java
@@ -1,31 +1,41 @@
-package org.kaazing.gateway.transport.http.specification;
+/**
+ * Copyright 2007-2015, Kaazing Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import static org.junit.rules.RuleChain.outerRule;
-import static java.util.concurrent.TimeUnit.SECONDS;
-
-import java.io.File;
-import java.net.URI;
+package org.kaazing.gateway.service.http.directory.specification;
 
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.DisableOnDebug;
 import org.junit.rules.TestRule;
-import org.junit.rules.Timeout;
 import org.kaazing.gateway.server.test.GatewayRule;
 import org.kaazing.gateway.server.test.config.GatewayConfiguration;
 import org.kaazing.gateway.server.test.config.builder.GatewayConfigurationBuilder;
 import org.kaazing.k3po.junit.annotation.Specification;
 import org.kaazing.k3po.junit.rules.K3poRule;
-import org.kaazing.test.util.MethodExecutionTrace;
+
+import java.io.File;
+import java.net.URI;
+
+import static org.kaazing.test.util.ITUtil.createRuleChain;
 
 public class Rfc7235SpecificationIT {
 
-    private TestRule trace = new MethodExecutionTrace();
-    private TestRule timeout = new DisableOnDebug(new Timeout(4, SECONDS));
     private final K3poRule robot = new K3poRule().setScriptRoot("org/kaazing/specification/http/rfc7235");
 
-    public GatewayRule gateway = new GatewayRule() { 
+    private final GatewayRule gateway = new GatewayRule() {
        {
            GatewayConfiguration configuration = new GatewayConfigurationBuilder()
                     .webRootDirectory(new File("src/test/webapp"))
@@ -61,7 +71,7 @@ public class Rfc7235SpecificationIT {
     };
 
     @Rule
-    public TestRule chain = outerRule(trace).around(robot).around(gateway).around(timeout);
+    public TestRule chain = createRuleChain(gateway, robot);
 
     @Test
     @Specification("status/valid.credentials/request")
@@ -70,7 +80,7 @@ public class Rfc7235SpecificationIT {
     }
 
     @Test
-    @Specification("status/multiple.invalid.requests/request")
+    @Specification("status/multiple.requests.with.invalid.credentials/request")
     public void shouldRespondWithMultiple401sWithMultipleInvalidRequests() throws Exception {
         robot.finish();
     }
