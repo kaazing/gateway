43b0660a2d2d24bdf31c38f5602ff3df78d18c3d
Jitendra Kotamraju <jitsni@gmail.com>
2016-02-03 12:21:33 -0800

    Fixing license headers

--- transport/wsn/src/test/java/org/kaazing/gateway/transport/wsn/httpx/OpeningHandShakeIT.java
+++ transport/wsn/src/test/java/org/kaazing/gateway/transport/wsn/httpx/OpeningHandShakeIT.java
@@ -1,26 +1,36 @@
+/**
+ * Copyright 2007-2015, Kaazing Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.kaazing.gateway.transport.wsn.httpx;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.junit.rules.RuleChain.outerRule;
+import static org.kaazing.test.util.ITUtil.createRuleChain;
 
 import java.net.URI;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.DisableOnDebug;
 import org.junit.rules.TestRule;
-import org.junit.rules.Timeout;
 import org.kaazing.gateway.server.test.GatewayRule;
 import org.kaazing.gateway.server.test.config.GatewayConfiguration;
 import org.kaazing.gateway.server.test.config.builder.GatewayConfigurationBuilder;
 import org.kaazing.k3po.junit.annotation.Specification;
 import org.kaazing.k3po.junit.rules.K3poRule;
-import org.kaazing.test.util.MethodExecutionTrace;
 
 public class OpeningHandShakeIT {
 
-    private TestRule trace = new MethodExecutionTrace();
-    private TestRule timeout = new DisableOnDebug(new Timeout(4, SECONDS));
     private final K3poRule robot = new K3poRule().setScriptRoot("org/kaazing/specification/httpx/opening");
 
     public GatewayRule gateway = new GatewayRule() {
@@ -40,7 +50,7 @@ public class OpeningHandShakeIT {
     };
 
     @Rule
-    public TestRule chain = outerRule(trace).around(robot).around(gateway).around(timeout);
+    public TestRule chain = createRuleChain(gateway, robot);
 
     @Test
     @Specification("extended.handshake.protocol.negotiated/request")
