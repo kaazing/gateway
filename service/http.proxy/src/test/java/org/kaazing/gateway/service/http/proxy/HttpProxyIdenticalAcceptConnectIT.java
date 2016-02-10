7e7e09160bb1df2290912c0c3e9216b1d4fe7ec2
Irina Mitrea <imitrea@luxoft.com>
2016-02-10 14:45:57 +0200

    Added minor change to test

--- service/http.proxy/src/test/java/org/kaazing/gateway/service/http/proxy/HttpProxyIdenticalAcceptConnectIT.java
+++ service/http.proxy/src/test/java/org/kaazing/gateway/service/http/proxy/HttpProxyIdenticalAcceptConnectIT.java
@@ -17,6 +17,7 @@ package org.kaazing.gateway.service.http.proxy;
 
 import java.net.URI;
 
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -24,21 +25,42 @@ import org.kaazing.gateway.server.test.Gateway;
 import org.kaazing.gateway.server.test.config.builder.GatewayConfigurationBuilder;
 
 public class HttpProxyIdenticalAcceptConnectIT {
+    private Gateway gateway = new Gateway();
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
+
+    @After
+    public void clear() throws Exception {
+        gateway.stop();
+    }
 
     @Test
     public void shouldFailWhenIdenticalAcceptAndConnect() throws Exception {
         thrown.expect(RuntimeException.class);
         thrown.expectMessage("Different <accept> and <connect> URIs should be provided for service of type http.proxy");
-        Gateway gateway = new Gateway();
         // @formatter:off
         gateway.start(new GatewayConfigurationBuilder()
                 .service()
                 .accept(URI.create("http://localhost:8080/"))
+                .accept(URI.create("http://localhost:8081/"))
                 .connect(URI.create("http://localhost:8080/"))
                 .type("http.proxy")
+            .done()
+        .done());
+        // @formatter:on
+    }
+
+    @Test
+    public void shouldNotFailWhenIdenticalAcceptAndConnectOptions() throws Exception {
+        // @formatter:off
+        gateway.start(new GatewayConfigurationBuilder()
+                .service()
+                .accept(URI.create("http://localhost:8080/"))
+                .accept(URI.create("http://localhost:8081/"))
+                .connect(URI.create("http://localhost:8080/"))
+                .type("http.proxy")
+                .connectOption("http.transport", "tcp://localhost:8081")
             .done()
         .done());
         // @formatter:on
