228289b845c8fcb84b5f9fc4b3efc7b764b0f95f
Cojanu <ACojanu@romania.luxoft.com>
2016-02-15 16:42:08 +0200

    Moved DuplicateBindTest (*gateway/server/transport/wseb -> *gateway/transport/wseb)

--- transport/wseb/src/test/java/org/kaazing/gateway/transport/wseb/DuplicateBindTest.java
+++ transport/wseb/src/test/java/org/kaazing/gateway/transport/wseb/DuplicateBindTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kaazing.gateway.server.transport.wseb;
+package org.kaazing.gateway.transport.wseb;
 
 import static org.junit.Assert.assertFalse;
 
