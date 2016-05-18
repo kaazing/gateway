d3edae9c8864cecce5f9699dfb5467d7963a9bab
Jitendra Kotamraju <jitsni@gmail.com>
2016-05-18 16:20:18 -0700

    Removing a type parameter that hides visible type

--- resource.address/spi/src/main/java/org/kaazing/gateway/resource/address/ResourceAddressFactorySpi.java
+++ resource.address/spi/src/main/java/org/kaazing/gateway/resource/address/ResourceAddressFactorySpi.java
@@ -426,15 +426,13 @@ public abstract class ResourceAddressFactorySpi<T extends ResourceAddress> {
                                       Map<String, Object> optionsByName) {
     }
 
-    protected <T extends ResourceAddress> T newResourceAddressWithAlternate(String location,
-                                                                            Map<String, Object> optionsByName,
-                                                                            final ResourceAddress alternateAddress) {
+    @SuppressWarnings("unchecked")
+    protected T newResourceAddressWithAlternate(String location,
+                                                Map<String, Object> optionsByName,
+                                                ResourceAddress alternateAddress) {
 
         optionsByName.put(ALTERNATE.name(), alternateAddress);
-        return (T)
-                getResourceAddressFactory().
-                        newResourceAddress(location, optionsByName);
-
+        return (T) getResourceAddressFactory().newResourceAddress(location, optionsByName);
     }
 
     /**
