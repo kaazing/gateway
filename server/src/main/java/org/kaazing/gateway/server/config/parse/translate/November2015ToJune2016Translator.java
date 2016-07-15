7bad28839ab2518b328789f6bf656f3af1aa5fbf
Justin Ma <justinma246@gmail.com>
2016-07-15 10:58:22 -0700

    Revert "Changes"
    This reverts commit 45fd36ab3a19674e90cf1a77b6babaffb73485e2, reversing
    changes made to a436c35bd07446f475f209d8bb15178cb4d0a7b0.

--- server/src/main/java/org/kaazing/gateway/server/config/parse/translate/November2015ToJune2016Translator.java
+++ server/src/main/java/org/kaazing/gateway/server/config/parse/translate/November2015ToJune2016Translator.java
@@ -15,26 +15,20 @@
  */
 package org.kaazing.gateway.server.config.parse.translate;
 
-<<<<<<< HEAD:server/src/main/java/org/kaazing/gateway/server/config/parse/translate/November2015ToJune2016Translator.java
 import org.kaazing.gateway.server.config.parse.GatewayConfigNamespace;
 import org.kaazing.gateway.server.config.parse.translate.june2016.RemoveRealmVisitor;
-=======
->>>>>>> 2a3577a9766d8d9c435fa6eb618fd2c5d6c0bb27:server/src/main/java/org/kaazing/gateway/server/config/parse/translate/November2015Validator.java
 import org.kaazing.gateway.server.config.parse.translate.sep2014.FindMatchingBalancerServiceVisitor;
 
-public class November2015Validator extends GatewayConfigTranslatorPipeline {
-    public November2015Validator() {
+public class November2015ToJune2016Translator extends GatewayConfigTranslatorPipeline {
+    public November2015ToJune2016Translator() {
         super();
 
         // for each balance URI, make sure there is a corresponding balancer service accepting on that URI
         // for each balancer service accept URI, make sure there is a corresponding balance URI pointing to that service
         addTranslator(new FindMatchingBalancerServiceVisitor());
-<<<<<<< HEAD:server/src/main/java/org/kaazing/gateway/server/config/parse/translate/November2015ToJune2016Translator.java
         
         addTranslator(new NamespaceVisitor(GatewayConfigNamespace.CURRENT_NS));
         
         addTranslator(new RemoveRealmVisitor());
-=======
->>>>>>> 2a3577a9766d8d9c435fa6eb618fd2c5d6c0bb27:server/src/main/java/org/kaazing/gateway/server/config/parse/translate/November2015Validator.java
     }
 }
