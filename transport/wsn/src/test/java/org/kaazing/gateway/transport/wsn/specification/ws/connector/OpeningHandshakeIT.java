3f4aee01a0aaf05f8f7ea5c00cf1970dbd01fd0d
Doru-Mihai Muntean <dorumihai.muntean@kaazing.com>
2016-07-18 16:33:03 +0300

    Checking if 'Connection' header is present when Gateway connects as a client. Up to now, if connection header was not present, the connection would still be established

--- transport/wsn/src/test/java/org/kaazing/gateway/transport/wsn/specification/ws/connector/OpeningHandshakeIT.java
+++ transport/wsn/src/test/java/org/kaazing/gateway/transport/wsn/specification/ws/connector/OpeningHandshakeIT.java
@@ -285,9 +285,7 @@ public class OpeningHandshakeIT {
     }
 
     @Test
-    @Ignore("Issue# 311: Missing Connection header in handshake response. connectFuture.isConnected() must return false")
-    @Specification({
-        "response.header.connection.missing/handshake.response" })
+    @Specification({"response.header.connection.missing/handshake.response"})
     public void shouldFailConnectionWhenResponseHeaderConnectionMissing() throws Exception {
         final IoHandler handler = context.mock(IoHandler.class);
 
