cdf1a21c04780d942f60dba6832d678e09a3bc28
claudiaop <claudia.oprescu@kaazing.com>
2016-08-10 12:37:18 +0300

    modified http proxy test for sending another port in header

--- service/http.proxy/src/test/java/org/kaazing/gateway/service/http/proxy/HttpProxyConnectionDifferentPortIT.java
+++ service/http.proxy/src/test/java/org/kaazing/gateway/service/http/proxy/HttpProxyConnectionDifferentPortIT.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2007-2016, Kaazing Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.kaazing.gateway.service.http.proxy;
 
 import static org.kaazing.test.util.ITUtil.createRuleChain;
@@ -33,6 +49,7 @@ public class HttpProxyConnectionDifferentPortIT {
                             .service()
                             .accept("http://localhost:8111")
                             .connect("http://localhost:8081")
+                            .name("Proxy Service 2")
                             .type("http.proxy")
                             .done()
                             .done();
@@ -46,7 +63,7 @@ public class HttpProxyConnectionDifferentPortIT {
 
 
     @Test
-    @Specification("test/http.test")
+	@Specification("http.proxy.multiple.services.different.port")
     public void httppath() throws Exception {
         k3po.finish();
     }
