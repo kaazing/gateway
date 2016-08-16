7a56ea1c02d54f2ecf207df8638688ff2a3733f4
Costin Chiriac <costin.chiriac@kaazing.com>
2016-08-16 14:20:03 +0300

    Small improvements of turn utility class.

--- util/src/main/java/org/kaazing/gateway/util/turn/TurnUtils.java
+++ util/src/main/java/org/kaazing/gateway/util/turn/TurnUtils.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2007-2016, Kaazing Corporation. All rights reserved.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,16 +32,30 @@ import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
 public class TurnUtils {
+
+    private TurnUtils() {
+        // utility class should hide constructor
+    }
+
     public static Key getSharedSecret(KeyStore ks, String alias, File pwFile) {
+        char[] password;
         try {
-            char[] password = loadKeyStorePassword(pwFile);
+            password = loadKeyStorePassword(pwFile);
+        } catch (IOException e) {
+            throw new TurnException("Unable to load password from file: " + pwFile, e);
+        }
+        return getSharedSecret(ks, alias, password);
+    }
+
+    public static Key getSharedSecret(KeyStore ks, String alias, char[] password) {
+        try {
             return ks.getKey(alias, password);
-        } catch (UnrecoverableKeyException | KeyStoreException | NoSuchAlgorithmException | IOException e) {
+        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
             throw new TurnException("Unable to get shared secret", e);
         }
     }
 
-    public static char[] getPassword(String username, Key sharedSecret, String algorithm) {
+    public static byte[] getPassword(String username, Key sharedSecret, String algorithm) {
         byte[] key = sharedSecret.getEncoded();
         Mac hmac;
         try {
@@ -52,10 +66,16 @@ public class TurnUtils {
             throw new TurnException("Unable to generate password", e);
         }
 
-        return Base64.getEncoder().encodeToString(hmac.doFinal(username.getBytes())).toCharArray();
+
+        return hmac.doFinal(username.getBytes());
     }
 
-    private static char[] loadKeyStorePassword(File location) throws IOException{
+    public static char[] getBase64EncodedPassword(String username, Key sharedSecret, String algorithm) {
+        byte[] password = getPassword(username, sharedSecret, algorithm);
+        return Base64.getEncoder().encodeToString(password).toCharArray();
+    }
+
+    private static char[] loadKeyStorePassword(File location) throws IOException {
         try (BufferedReader in = new BufferedReader(new FileReader(location))) {
             String line = in.readLine();
             return line.toCharArray();
