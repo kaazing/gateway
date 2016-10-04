4ef2b273a20c04def0af12bb3704749ab004bae7
Alex Pirvu <apirvu@luxoft.com>
2016-10-04 18:23:28 +0300

    Implemented logic+sharedState'

--- service/http.directory/src/test/java/org/kaazing/gateway/service/http/directory/ExpiringTokenCustomLoginModule.java
+++ service/http.directory/src/test/java/org/kaazing/gateway/service/http/directory/ExpiringTokenCustomLoginModule.java
@@ -1,3 +1,4 @@
+
 /**
  * Copyright 2007-2016, Kaazing Corporation. All rights reserved.
  *
@@ -15,20 +16,26 @@
  */
 package org.kaazing.gateway.service.http.directory;
 
+import java.io.File;
 import java.io.IOException;
 import java.security.Principal;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
 
 import javax.security.auth.Subject;
-import javax.security.auth.callback.Callback;
-import javax.security.auth.callback.CallbackHandler;
-import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.callback.*;
 import javax.security.auth.login.FailedLoginException;
 import javax.security.auth.login.LoginException;
 import javax.security.auth.spi.LoginModule;
 
+
+import org.kaazing.gateway.security.auth.config.JaasConfig;
+import org.kaazing.gateway.security.auth.config.RoleConfig;
+import org.kaazing.gateway.security.auth.config.UserConfig;
+import org.kaazing.gateway.security.auth.config.parse.DefaultUserConfig;
+import org.kaazing.gateway.security.auth.config.parse.JaasConfigParser;
 import org.kaazing.gateway.server.ExpiringState;
 import org.kaazing.gateway.server.spi.security.AuthenticationToken;
 import org.kaazing.gateway.server.spi.security.AuthenticationTokenCallback;
@@ -38,188 +45,175 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExpiringTokenCustomLoginModule implements LoginModule {
+
     public static final String CLASS_NAME = ExpiringTokenCustomLoginModule.class.getName();
     public static final String LOG_PREFIX = "[LM] ";
     public static final Logger logger = LoggerFactory.getLogger(CLASS_NAME);
     private static final String NUMBER_OF_ATTEMPTS_KEY = "NUMBER_OF_ATTEMPTS";
-    private Subject subject;
-    private CallbackHandler handler;
-    private Map<String, ?> options;
-    private UserPrincipal userPrincipal;
-    private RolePrincipal rolePrincipal;
+
+    private static final String TEST_PRINCIPAL_PASS = "testPrincipalPass";
+    private static final String TEST_PRINCIPAL_NAME = "testPrincipalName";
+    private DefaultUserConfig defaultPrincipal = new DefaultUserConfig();
+
     private ExpiringState expiringState;
 
+    // initial state
+    protected Subject subject;
+    private Map<String, ?> sharedState;
+
+    // the authentication status
+    private boolean succeeded;
+    private boolean commitSucceeded;
+
+    // testUser's RolePrincipal
+    private RolePrincipal userPrincipal;
+
     @Override
-    public void initialize(Subject subject, CallbackHandler callbackHandler, Map<String, ?> sharedState, Map<String, ?> options) {
-        logDebug("initialize()");
+    public void initialize(Subject subject,
+                           CallbackHandler callbackHandler,
+                           Map<String, ?> sharedState,
+                           Map<String, ?> options) {
 
-        this.subject = subject;
-        this.handler = callbackHandler;
-        this.options = options;
-
-        Set<?> keys = this.options.keySet();
-        if (keys.size() > 0) {
-            logDebug("Options:");
-            for (Object key : keys) {
-                logDebug("  " + key + "=" + this.options.get(key));
-            }
-        }
         expiringState = (ExpiringState) options.get("ExpiringState");
-
-    }
-
-    @Override
-    public boolean login() throws LoginException {
-
-        Integer numOfAttempts = (Integer) expiringState.get(NUMBER_OF_ATTEMPTS_KEY);
-        if(numOfAttempts != null || numOfAttempts > 0){
-            // pass login send 200
-            // TO CONFIRM I think you return true
-            return true;
-        }
-        expiringState.putIfAbsent(NUMBER_OF_ATTEMPTS_KEY, new Integer(1), 10, TimeUnit.SECONDS);
-        //fail login / send 401
-        logDebug("401 - login failed!!");
-
-        return false;
-    }
-
-    @Override
-    public boolean commit() throws LoginException {
-        logDebug("commit()");
-        if (this.userPrincipal == null) {
-            logDebug("No user principal added");
-            return false;
-        }
-        this.subject.getPrincipals().add(this.userPrincipal);
-
-        logDebug("User principal added: " + this.userPrincipal.getName());
-
-        if (this.rolePrincipal == null) {
-            logDebug("No role granted");
-            return false;
-        }
-        this.subject.getPrincipals().add(this.rolePrincipal);
-
-        logDebug("User granted role: " + this.rolePrincipal.getName());
-
-        Set<Principal> principals = this.subject.getPrincipals();
-        for (Principal principal : principals) {
-            logDebug("Principal: " + principal.getName() + " [" + principal.getClass().getName() + "]");
-        }
-        return true;
-    }
-
-    @Override
-    public boolean abort() throws LoginException {
-        logDebug("abort()");
-        if (this.userPrincipal == null) {
-            logDebug("No user principal");
-            return false;
-        }
-        if (this.rolePrincipal == null) {
-            logDebug("No role granted");
-            return false;
-        }
-        try {
-            clearRole();
-
-            return true;
-        } catch (Exception ex) {
-            LoginException e = new LoginException("Unexpected error during abort().");
-            e.initCause(ex);
-            logError(e.getMessage(), e);
-            throw e;
-        }
-    }
-
-    @Override
-    public boolean logout() throws LoginException {
-        logDebug("logout()");
-        try {
-            clearRole();
-
-            return true;
-        } catch (Exception ex) {
-            LoginException e = new LoginException("Unexpected error during logout().");
-            e.initCause(ex);
-            logError(e.getMessage(), e);
-            throw e;
-        }
+        this.subject = subject;
+        this.sharedState = sharedState;
     }
 
     private void logDebug(String message) {
         logger.debug(LOG_PREFIX + message);
     }
 
-    private void logError(String message, Exception exception) {
-        logger.error(LOG_PREFIX + message, exception);
-    }
 
-    private LoginResult getLoginResultFromCallback() {
-        LoginResultCallback loginResultCallback = new LoginResultCallback();
-        try {
-            this.handler.handle(new Callback[]{loginResultCallback});
-        } catch (IOException ioe) {
-            logError("Encountered exception handling loginResultCallback", ioe);
-            return null;
-        } catch (UnsupportedCallbackException uce) {
-            logError("UnsupportedCallbackException handling loginResultCallback", uce);
-            return null;
-        }
-        return loginResultCallback.getLoginResult();
-    }
-
-    private AuthenticationToken getTokenFromCallback() {
-        AuthenticationTokenCallback authTokenCallback = new AuthenticationTokenCallback();
-        try {
-            this.handler.handle(new Callback[]{authTokenCallback});
-        } catch (IOException ioe) {
-            logError("Encountered exception handling authTokenCallback", ioe);
-            return null;
-        } catch (UnsupportedCallbackException uce) {
-            logError("UnsupportedCallbackException handling authenticationTokenCallback", uce);
-            return null;
-        }
-        AuthenticationToken authToken = authTokenCallback.getAuthenticationToken();
-
-        return authToken;
-    }
-
-    private String isTokenValid(String tokenData) throws FailedLoginException {
-        if (tokenData == null) {
-            throw new FailedLoginException("No Siteminder Token available to validate");
+    @Override
+    public boolean login() throws LoginException {
+        // verify the username/password
+        String username = (String) sharedState.get("javax.security.auth.login.name");
+        char[] password = (char[]) sharedState.get("javax.security.auth.login.password");
+        if (username == null || password == null) {
+            throw new FailedLoginException("No UserName/Password to authenticate");
         }
 
-        if (tokenData.compareToIgnoreCase("trump") == 0) {
-            throw new FailedLoginException("This user is not authorized");
+        if (username.equals("joe") && password.length == 7 && password[0] == 'w' && password[1] == 'e'
+                && password[2] == 'l' && password[3] == 'c' && password[4] == 'o' && password[5] == 'm'
+                && password[6] == 'e') {
+            // authentication succeeded!!!
+            Integer numOfAttempts = null;
+            if ( expiringState != null ) {
+                Object o = expiringState.get(NUMBER_OF_ATTEMPTS_KEY);
+                if (o!=null) {
+                    numOfAttempts = (Integer) o;
+                    logDebug("numOfAttempts="+numOfAttempts);
+                } else {
+                    logDebug("o is null");
+                }
+            } else {
+                logDebug("expiringState is null");
+            }
+
+            if(numOfAttempts != null && numOfAttempts > 0){
+                logDebug("login successful");
+                // pass login send 200
+                // TO CONFIRM I think you return true
+                succeeded = true;
+                return true;
+            }
+
+            expiringState.putIfAbsent(NUMBER_OF_ATTEMPTS_KEY, new Integer(1), 10, TimeUnit.SECONDS);
+            //fail login / send 401
+            logDebug("401 - login failed!!");
+            succeeded = false;
+            return false;
+
+
+        } else {
+            logDebug("UserName/Password is Incorrect");
+            // authentication failed
+            succeeded = false;
+            throw new FailedLoginException("UserName/Password is Incorrect");
+        }
+    }
+
+    @Override
+    public boolean commit() throws LoginException {
+        if (!succeeded) {
+            return false;
+        } else {
+            // add a Principal (authenticated identity) to the Subject
+            userPrincipal = new RolePrincipal("AUTHORIZED");
+            subject.getPrincipals().add(userPrincipal);
+            commitSucceeded = true;
+
+            defaultPrincipal.setName(TEST_PRINCIPAL_NAME);
+            defaultPrincipal.setPassword(TEST_PRINCIPAL_PASS);
+            subject.getPrincipals().add(defaultPrincipal);
+
+            return true;
+        }
+    }
+
+    @Override
+    public boolean abort() throws LoginException {
+        if (!succeeded) {
+            return false;
+        } else if (!commitSucceeded) {
+            // login succeeded but overall authentication failed
+            succeeded = false;
+            userPrincipal = null;
+        } else {
+            // overall authentication succeeded and commit succeeded,
+            // but someone else's commit failed
+            logout();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean logout() throws LoginException {
+        subject.getPrincipals().remove(userPrincipal);
+        succeeded = false;
+        commitSucceeded = false;
+        userPrincipal = null;
+        subject.getPrincipals().remove(defaultPrincipal);
+        return true;
+    }
+
+    private static class RolePrincipal implements Principal {
+
+        private final String name;
+
+        public RolePrincipal(String name) {
+            this.name = name;
         }
 
-        return tokenData;
-    }
-
-    private void clearRole() {
-        this.subject.getPrincipals().remove(this.userPrincipal);
-        this.subject.getPrincipals().remove(this.rolePrincipal);
-    }
-
-    static class RolePrincipal implements Principal {
         @Override
         public String getName() {
-            return "TEST";
-        }
-    }
-
-    static class UserPrincipal implements Principal {
-        String username;
-
-        public UserPrincipal(String username) {
-            this.username = username;
+            return name;
         }
 
         @Override
-        public String getName() {
-            return username;
+        public String toString() {
+            return "Role:  " + name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null) {
+                return false;
+            }
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof RolePrincipal)) {
+                return false;
+            }
+            RolePrincipal that = (RolePrincipal) o;
+
+            return this.getName().equals(that.getName());
+        }
+
+        @Override
+        public int hashCode() {
+            return name.hashCode();
         }
     }
 
