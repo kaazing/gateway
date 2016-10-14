39606b76a89eb411db14770c5e54057af02b4c8d
Roman Smolgovsky <roman@nemiga.net>
2016-10-13 17:11:44 -0700

    Updated the Principal. Added the copyright.

--- security/src/main/java/org/kaazing/gateway/security/auth/GrantLoginModule.java
+++ security/src/main/java/org/kaazing/gateway/security/auth/GrantLoginModule.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2007-2016, Kaazing Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kaazing.gateway.security.auth;
 
 import java.security.Principal;
@@ -11,20 +26,16 @@ import javax.security.auth.spi.LoginModule;
 public class GrantLoginModule implements LoginModule {
 	private Principal principal;
 	private Subject subject;
-	private String grantRole;
+	private String name;
 	@Override
 	public void initialize(Subject subject, CallbackHandler callbackHandler, Map<String, ?> sharedState, Map<String, ?> options) {
 		this.subject=subject;
-		this.grantRole = (String) options.get("grant.role");
+		this.name = (String) options.get("name");
 	}
 
 	@Override
 	public boolean login() throws LoginException {
-		this.principal = new Principal() {
-			public String getName() {
-				return grantRole;
-			}
-		};
+		this.principal = new GrantPrincipal(this.name);
 
 		return true;
 	}
