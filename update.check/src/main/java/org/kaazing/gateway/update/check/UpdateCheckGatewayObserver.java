d988c6efd5d2dc9cd986915765bd5f9ee4955d68
zahariaca <zaharia.c.alexandru@gmail.com>
2017-04-06 14:11:37 +0300

    Small formatting change

--- update.check/src/main/java/org/kaazing/gateway/update/check/UpdateCheckGatewayObserver.java
+++ update.check/src/main/java/org/kaazing/gateway/update/check/UpdateCheckGatewayObserver.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2007-2016, Kaazing Corporation. All rights reserved.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -85,7 +85,8 @@ public class UpdateCheckGatewayObserver implements GatewayObserverFactorySpi {
         listeners.add(newListener);
     }
 
-    @Override public void startingGateway(GatewayContext gatewayContext) {
+    @Override
+    public void startingGateway(GatewayContext gatewayContext) {
         Map<String, Object> injectables = gatewayContext.getInjectables();
         Properties properties = (Properties) injectables.get("configuration");
         if (!InternalSystemProperty.UPDATE_CHECK.getBooleanProperty(properties)) {
